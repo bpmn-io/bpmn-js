f3f95154fdee4af0fb1a7126ec62e6ca2e1d5ec1
Nico Rehwaldt <git_nikku@nixis.de>
2014-03-13 20:21:42 +0100

    feat(import): import collaboration + artifacts + message flows
    This commit adds support for collaborations, message flows and
    artifacts.
    
    Related to #1

--- test/spec/node/import/BpmnTreeWalkerSpec.js
+++ test/spec/node/import/BpmnTreeWalkerSpec.js
@@ -21,76 +21,159 @@ describe('BpmnTreeWalker', function() {
     return read(readBpmnDiagram(file), root, opts, callback);
   }
 
+
   beforeEach(Matchers.add);
 
   var bpmnModel = BpmnModel.instance();
 
+  describe('should walk', function() {
 
-  it('should walk simple process', function(done) {
+    /**
+     * Expect a certain render order when parsing the given file contents.
+     * 
+     * Call done when done.
+     */
+    function expectWalkOrder(file, expectedOrder, done) {
 
-    readFile('simple.bpmn', 'bpmn:Definitions', function(err, definitions) {
+      readFile(file, 'bpmn:Definitions', function(err, definitions) {
 
-      if (err) {
-        done(err);
-      } else {
+        if (err) {
+          done(err);
+          return;
+        }
 
-        var drawn = [];
+        var actualOrder = [];
 
         var visitor = function(element, di, ctx) {
           var id = element.id;
 
-          drawn.push({ id: id, parent: ctx });
+          actualOrder.push({ id: id, parent: ctx });
 
           return id;
         };
 
         new BpmnTreeWalker(visitor).handleDefinitions(definitions);
 
-        var expectedDrawn = [
-          { id: 'SubProcess_1', parent: undefined },
-          { id: 'StartEvent_1', parent: 'SubProcess_1' },
-          { id: 'Task_1', parent: 'SubProcess_1' },
-          { id: 'SequenceFlow_1', parent: 'SubProcess_1' } ];
-
-
-        expect(drawn).toDeepEqual(expectedDrawn);
+        expect(actualOrder).toDeepEqual(expectedOrder);
 
         done();
-      }
+      });
+    }
+
+    it('process / sub process', function(done) {
+
+      // given
+      var expectedOrder = [
+        { id: 'SubProcess_1', parent: undefined },
+        { id: 'StartEvent_1', parent: 'SubProcess_1' },
+        { id: 'Task_1', parent: 'SubProcess_1' },
+        { id: 'SequenceFlow_1', parent: 'SubProcess_1' } ];
+
+      // then
+      expectWalkOrder('simple.bpmn', expectedOrder, done);
     });
-  });
 
-  it('should walk collaboration', function(done) {
+    it('process elements', function(done) {
 
-    readFile('collaboration.bpmn', 'bpmn:Definitions', function(err, definitions) {
+      // given
+      var expectedOrder = [
+        { id : 'ParallelGateway_1', parent : undefined },
+        { id : 'ServiceTask_1', parent : undefined },
+        { id : 'DataStoreReference_2', parent : undefined },
+        { id : 'CallActivity_1', parent : undefined },
+        { id : 'StartEvent_2', parent : undefined },
+        { id : 'ExclusiveGateway_1', parent : undefined },
+        { id : 'SubProcess_1', parent : undefined },
+        { id : 'StartEvent_1', parent : 'SubProcess_1' },
+        { id : 'Task_1', parent : 'SubProcess_1' },
+        { id : 'BoundaryEvent_1', parent : 'SubProcess_1' },
+        { id : 'SequenceFlow_1', parent : 'SubProcess_1' },
+        { id : 'SequenceFlow_7', parent : 'SubProcess_1' },
+        { id : 'SequenceFlow_6', parent : undefined },
+        { id : 'SequenceFlow_2', parent : undefined },
+        { id : 'SequenceFlow_4', parent : undefined },
+        { id : 'SequenceFlow_5', parent : undefined },
+        { id : 'SequenceFlow_3', parent : undefined }
+      ];
 
-      if (err) {
-        done(err);
-      } else {
-
-        var drawn = [];
-
-        var visitor = function(element, di, ctx) {
-          var id = element.id;
-
-          drawn.push({ id: id, parent: ctx });
-
-          return id;
-        };
-        
-        new BpmnTreeWalker(visitor).handleDefinitions(definitions);
-
-        var expectedDrawn = [
-          { id : '_Participant_2', parent : undefined },
-          { id : 'Task_1', parent : '_Participant_2' },
-          { id : 'Participant_1', parent : undefined },
-          { id : 'StartEvent_1', parent : 'Participant_1' } ];
-
-
-        expect(drawn).toDeepEqual(expectedDrawn);
-
-        done();
-      }
+      expectWalkOrder('process-elements.bpmn', expectedOrder, done);
     });
+
+    it('associations / data objects', function(done) {
+
+      // given
+      var expectedOrder = [
+        { id : 'Task_1', parent : undefined },
+        { id : 'DataInputAssociation_1', parent : null },
+        { id : 'DataOutputAssociation_1', parent : null },
+        { id : 'BoundaryEvent_1', parent : undefined },
+        { id : 'Task_2', parent : undefined },
+        { id : 'DataInputAssociation_2', parent : null },
+        { id : 'DataOutputAssociation_2', parent : null },
+        { id : 'DataStoreReference_4', parent : undefined },
+        { id : 'DataObjectReference_1', parent : undefined },
+        { id : 'DataInput_1', parent : undefined },
+        { id : 'DataOutput_1', parent : undefined },
+        { id : 'Association_1', parent : undefined },
+        { id : 'Association_2', parent : undefined }
+      ];
+
+      expectWalkOrder('process-associations.bpmn', expectedOrder, done);
+    });
+
+    it('collaboration', function(done) {
+
+      // given
+      var expectedOrder = [
+        { id : 'Participant_2', parent : undefined },
+        { id : 'Lane_1', parent : 'Participant_2' },
+        { id : 'Lane_2', parent : 'Lane_1' },
+        { id : 'Lane_3', parent : 'Lane_1' },
+        { id : 'Task_1', parent : 'Lane_3' },
+        { id : 'Participant_1', parent : undefined },
+        { id : 'StartEvent_1', parent : 'Participant_1' }
+      ];
+
+      expectWalkOrder('collaboration.bpmn', expectedOrder, done);
+    });
+    
+    it('collaboration / message flows', function(done) {
+
+      // given
+      var expectedOrder = [
+        { id : 'Participant_2', parent : undefined },
+        { id : 'Task_1', parent : 'Participant_2' },
+        { id : 'EndEvent_1', parent : 'Participant_2' },
+        { id : 'Participant_1', parent : undefined },
+        { id : 'StartEvent_1', parent : 'Participant_1' },
+        { id : 'MessageFlow_1', parent : undefined },
+        { id : 'MessageFlow_2', parent : undefined },
+        { id : 'MessageFlow_4', parent : undefined },
+        { id : 'MessageFlow_5', parent : undefined }
+      ];
+
+      expectWalkOrder('collaboration-message-flows.bpmn', expectedOrder, done);
+    });
+
+    it('collaboration / data items', function(done) {
+
+      // given
+      var expectedOrder = [
+        { id : 'Participant_2', parent : undefined },
+        { id : 'Lane_1', parent : 'Participant_2' },
+        { id : 'Lane_2', parent : 'Lane_1' },
+        { id : 'SubProcess_1', parent : 'Lane_2' },
+        { id : 'DataObjectReference_1', parent : 'SubProcess_1' },
+        { id : 'Lane_3', parent : 'Lane_1' },
+        { id : 'DataStoreReference_5', parent : undefined },
+        { id : 'DataStoreReference_6', parent : undefined },
+        { id : 'Participant_1', parent : undefined },
+        { id : 'DataInput_1', parent : 'Participant_1' },
+        { id : 'DataOutput_1', parent : 'Participant_1' }
+      ];
+
+      expectWalkOrder('collaboration-data-items.bpmn', expectedOrder, done);
+    });
+
   });
 });
