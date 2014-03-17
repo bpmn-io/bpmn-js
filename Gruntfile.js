1ab6b2b13b99d2ab11203d2cdcbf569bb45afb40
Nico Rehwaldt <git_nikku@nixis.de>
2014-03-17 15:11:15 +0100

    chore(build): generate api docs to docs/api

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,7 +112,7 @@ module.exports = function(grunt) {
       dist: {
         src: [ '<%= config.sources %>/**/*.js' ],
         options: {
-          destination: 'docs',
+          destination: 'docs/api',
           plugins: [ 'plugins/markdown' ]
         }
       }
