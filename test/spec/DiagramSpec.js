afa0c9e870b733f8047dce9c939445ca8bdb0fc6
Nico Rehwaldt <git_nikku@nixis.de>
2014-07-15 13:30:46 +0200

    feat(Canvas): return added shape/connection
    Instead of the canvas instance, return the added (and
    possibly extended) shape/connection in Canvas#addShape and
    Canvas#addConnection.
    
    Related to #41

--- test/spec/DiagramSpec.js
+++ test/spec/DiagramSpec.js
@@ -78,9 +78,8 @@ describe('diagram', function() {
         });
 
         diagram.invoke([ 'canvas', function(canvas) {
-          canvas
-            .addShape({ id: 's1', x: 10, y: 10, width: 30, height: 30 })
-            .addShape({ id: 's2', x: 100, y: 100, width: 30, height: 30 });
+          canvas.addShape({ id: 's1', x: 10, y: 10, width: 30, height: 30 });
+          canvas.addShape({ id: 's2', x: 100, y: 100, width: 30, height: 30 });
 
           canvas.addConnection({ id: 'c1', waypoints: [ { x: 25, y: 25 }, {x: 115, y: 115} ]});
         }]);
