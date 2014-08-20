4cb363064d2fce23939c12e795aa1aca1f683df6
jdotzki <joerg.dotzki@zalando.de>
2014-08-20 23:39:58 +0200

    feat(modeling): add #removeShape/Connection
    Added #removeShape and #removeConnection to modeling API.
    
    closes bpmn-io/bpmn-js#106

---
+++ test/spec/features/modeling/DeleteConnectionSpec.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var TestHelper = require('../../../TestHelper');
+
+/* global bootstrapDiagram, inject */
+
+
+var Matchers = require('../../../Matchers');
+
+
+var _ = require('lodash');
+
+var modelingModule = require('../../../../lib/features/modeling');
+
+
+describe('features/modeling - remove connection', function() {
+
+  beforeEach(Matchers.addDeepEquals);
+
+
+  beforeEach(bootstrapDiagram({ modules: [ modelingModule ] }));
+
+
+  var rootShape, parentShape, childShape, childShape2, connection;
+
+  beforeEach(inject(function(elementFactory, canvas, elementRegistry) {
+
+    rootShape = elementFactory.createRoot({
+      id: 'root'
+    });
+
+    parentShape = elementFactory.createShape({
+      id: 'parent',
+      x: 100, y: 100, width: 300, height: 300
+    });
+
+    canvas.addShape(parentShape, rootShape);
+
+    childShape = elementFactory.createShape({
+      id: 'child',
+      x: 110, y: 110, width: 100, height: 100
+    });
+
+    canvas.addShape(childShape, parentShape);
+
+    childShape2 = elementFactory.createShape({
+      id: 'child2',
+      x: 200, y: 110, width: 100, height: 100
+    });
+
+    canvas.addShape(childShape2, parentShape);
+
+    connection = elementFactory.createConnection({
+      id: 'connection',
+      waypoints: [ { x: 150, y: 150 }, { x: 150, y: 200 }, { x: 350, y: 150 } ],
+      source: childShape,
+      target: childShape2
+    });
+
+    canvas.addConnection(connection, parentShape);
+  }));
+
+
+  it('should remove connection', inject(function(modeling, elementRegistry) {
+
+    // when
+    modeling.removeConnection(connection);
+
+    // then
+    expect(connection.parent).toBeNull();
+  }));
+
+
+  it('should clean up incoming/outgoing on connected shapes ', inject(function(modeling, elementRegistry) {
+
+    // when
+    modeling.removeConnection(connection);
+
+    // then
+    expect(childShape.outgoing).not.toContain(connection);
+    expect(connection.source).toBeNull();
+
+    expect(childShape2.incoming).not.toContain(connection);
+    expect(connection.target).toBeNull();
+  }));
+
+
+  describe('undo support', function() {
+
+    it('should revert', inject(function (modeling, elementRegistry, commandStack) {
+
+      // given
+      modeling.removeConnection(connection);
+
+      // when
+      commandStack.undo();
+
+      // then
+      expect(connection.parent).toBe(parentShape);
+    }));
+
+
+    it('should restore correct source/target', inject(function (modeling, elementRegistry, commandStack) {
+
+      // given
+      modeling.removeConnection(connection);
+
+      // when
+      commandStack.undo();
+
+      // then
+      expect(childShape.outgoing).toContain(connection);
+      expect(connection.source).toBe(childShape);
+
+      expect(childShape2.incoming).toContain(connection);
+      expect(connection.target).toBe(childShape2);
+    }));
+
+
+    it('should restore reference after multiple undo', inject(function (modeling, elementRegistry, commandStack) {
+
+      // given
+      modeling.removeConnection(connection);
+      modeling.removeShape(childShape);
+
+      // when
+      commandStack.undo();
+      commandStack.undo();
+
+      // then
+      expect(childShape.outgoing).toContain(connection);
+      expect(childShape2.incoming).toContain(connection);
+    }));
+  });
+});
---
+++ test/spec/features/modeling/DeleteShapeSpec.js
@@ -0,0 +1,122 @@
+'use strict';
+
+var TestHelper = require('../../../TestHelper');
+
+/* global bootstrapDiagram, inject */
+
+
+var Matchers = require('../../../Matchers');
+
+
+var _ = require('lodash');
+
+var modelingModule = require('../../../../lib/features/modeling');
+
+
+describe('features/modeling - #removeShape', function() {
+
+  beforeEach(Matchers.addDeepEquals);
+
+
+  beforeEach(bootstrapDiagram({ modules: [ modelingModule ] }));
+
+
+  var rootShape, parentShape, childShape, childShape2, connection;
+
+  beforeEach(inject(function(elementFactory, canvas, elementRegistry) {
+
+    rootShape = elementFactory.createRoot({
+      id: 'root'
+    });
+
+    parentShape = elementFactory.createShape({
+      id: 'parent',
+      x: 100, y: 100, width: 300, height: 300
+    });
+
+    canvas.addShape(parentShape, rootShape);
+
+    childShape = elementFactory.createShape({
+      id: 'child',
+      x: 110, y: 110, width: 100, height: 100
+    });
+
+    canvas.addShape(childShape, parentShape);
+
+    childShape2 = elementFactory.createShape({
+      id: 'child2',
+      x: 200, y: 110, width: 100, height: 100
+    });
+
+    canvas.addShape(childShape2, parentShape);
+
+    connection = elementFactory.createConnection({
+      id: 'connection',
+      waypoints: [ { x: 150, y: 150 }, { x: 150, y: 200 }, { x: 350, y: 150 } ],
+      source: childShape,
+      target: childShape2
+    });
+
+    canvas.addConnection(connection, parentShape);
+  }));
+
+
+  it('should remove shape', inject(function(modeling, elementRegistry) {
+
+    // when
+    modeling.removeShape(childShape);
+
+    // then
+    expect(elementRegistry.getById(childShape.id)).not.toBeDefined();
+
+    expect(childShape.parent).toBeNull();
+    expect(parentShape.children).not.toContain(childShape);
+  }));
+
+
+  it('should remove incoming connection', inject(function(modeling, elementRegistry) {
+
+    // when
+    modeling.removeShape(childShape2);
+
+    // then
+    expect(connection.parent).toBeNull();
+  }));
+
+
+  it('should remove outgoing connection', inject(function(modeling, elementRegistry) {
+
+    // when
+    modeling.removeShape(childShape);
+
+    // then
+    expect(connection.parent).toBeNull();
+  }));
+
+
+  it('should remove children', inject(function(modeling, elementRegistry) {
+
+    // when
+    modeling.removeShape(parentShape);
+
+    // then
+    expect(parentShape.parent).toBeNull();
+    expect(childShape.parent).toBeNull();
+    expect(childShape2.parent).toBeNull();
+    expect(connection.parent).toBeNull();
+  }));
+
+
+  it('ensure revert works'), inject(function(modeling, elementRegistry, commandStack) {
+
+    // when
+    modeling.removeShape(childShape);
+
+    // when
+    commandStack.undo();
+
+    // then
+    expect(childShape.parent).toBe(parentShape);
+    expect(connection.parent).toBe(parentShape);
+  });
+});
