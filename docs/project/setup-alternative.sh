74900412d08d27445f210e5ed007215c9a5b4623
Nico Rehwaldt <git_nikku@nixis.de>
2014-08-26 11:26:55 +0200

    docs(project): remove inline setup script

--- docs/project/setup-alternative.sh
+++ docs/project/setup-alternative.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###
-# Setup script to be executed in a bpmn.io project root (some empty folder chosen by YOU)
+# Setup script to be executed in a bpmn.io project root (some empty folder chosen by YOU). Use if you do not want to rely on npm link.
 ###
 
 base=`pwd`
@@ -17,76 +17,72 @@ git clone git@github.com:bpmn-io/bpmn-js-cli.git
 git clone git@github.com:bpmn-io/bpmn-moddle.git
 git clone git@github.com:bpmn-io/ids.git
 
-echo done.
 
+echo done.
 
 echo setup diagram-js
 
 cd $base/diagram-js
 npm install
-npm link
 
 
 echo setup moddle
 
 cd $base/moddle
 npm install
-npm link
 
 
 echo setup moddle-xml
 
 cd $base/moddle-xml
-npm link moddle
+mkdir node_modules
+ln -s $base/moddle node_modules/moddle
 npm install
-npm link
 
 
 echo setup bpmn-moddle
 
 cd $base/bpmn-moddle
-npm link moddle-xml
-npm link moddle
+mkdir node_modules
+ln -s $base/moddle node_modules/moddle
+ln -s $base/moddle-xml node_modules/moddle-xml
 npm install
-npm link
 
 
 echo setup diagram-js-direct-editing
 
 cd $base/diagram-js-direct-editing
-npm link diagram-js
+mkdir node_modules
+ln -s $base/diagram-js node_modules/diagram-js
 npm install
-npm link
 
 
 echo setup ids
 
 cd $base/ids
 npm install
-npm link
 
 
 echo setup bpmn-js
 
 cd $base/bpmn-js
+mkdir node_modules
+ln -s $base/moddle node_modules/moddle
+ln -s $base/bpmn-js-cli node_modules/bpmn-js-cli
+ln -s $base/bpmn-moddle node_modules/bpmn-moddle
+ln -s $base/diagram-js node_modules/diagram-js
+ln -s $base/diagram-js-direct-editing node_modules/diagram-js-direct-editing
+ln -s $base/ids node_modules/ids
 npm install
-npm link diagram-js
-npm link diagram-js-direct-editing
-npm link ids
-npm link bpmn-moddle
-npm link
 
 
 echo setup bpmn-js-cli
 
 cd $base/bpmn-js-cli
+mkdir node_modules
+ln -s $base/bpmn-js node_modules/bpmn-js
 npm install
-npm link bpmn-js
-npm link
 
-# deferred link cli (circular dev dependency)
-cd $base/bpmn-js
-npm link bpmn-js-cli
 
 cd $base
 
