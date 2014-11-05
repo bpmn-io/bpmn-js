0de5adaca9a357ec50ed934b00340bece6e8fc6d
Nico Rehwaldt <git_nikku@nixis.de>
2014-11-05 14:13:20 +0100

    chore(assets): align stylesheet name
    This commit renames the diagram.css stylesheet to diagram-js to better
    align it with the project name.

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -3,4 +3,4 @@ var TestHelper = module.exports = require('./helper');
 var fs = require('fs');
 
 // insert diagram.css
-TestHelper.insertCSS('diagram.css', fs.readFileSync(__dirname + '/../assets/diagram.css', 'utf-8'));
+TestHelper.insertCSS('diagram-js.css', fs.readFileSync(__dirname + '/../assets/diagram-js.css', 'utf-8'));
