6181b1a5c36a87589e0ee70f95a31979e98d9404
jdotzki <joerg.dotzki@zalando.de>
2014-12-23 11:30:36 +0100

    feature(outline, selection): allow selection of connections
    Limitations: context pad is not functional for connections
    
    See bpmn-io/bpmn-js#121

---
+++ test/spec/features/outline/OutlineSpec.js
@@ -0,0 +1,104 @@
+'use strict';
+
+var TestHelper = require('../../../TestHelper');
+
+/* global bootstrapDiagram, inject */
+
+
+var selectionModule = require('../../../../lib/features/selection'),
+    graphicsModule = require('../../../../lib/features/selection'),
+    canvasModule = require('../../../../lib/features/selection');
+
+
+
+describe('features/outline/Outline', function() {
+
+  beforeEach(bootstrapDiagram({ modules: [ selectionModule, graphicsModule ] }));
+
+  describe('select', function() {
+
+    it('should add outline to shape', inject(function(selection, canvas, elementRegistry) {
+
+      // given
+      var shape = canvas.addShape({
+        id: 'test',
+        x: 10,
+        y: 10,
+        width: 100,
+        height: 100
+      });
+
+      // when
+      selection.select(shape);
+
+      // then
+      var gfx = elementRegistry.getGraphics(shape);
+      var outline = gfx.select('.djs-outline');
+
+
+      expect(outline).toBeTruthy(); // OUTLINE EXISTS
+      expect(gfx.hasClass('selected')).toBeTruthy(); // Outline class is set
+    }));
+
+    it('should add outline to connection', inject(function(selection, canvas, elementRegistry) {
+
+      // given
+      var connection = canvas.addConnection({ id: 'select1', waypoints: [ { x: 25, y: 25 }, {x: 115, y: 115} ]});
+
+      // when
+      selection.select(connection);
+
+      // then
+      var gfx = elementRegistry.getGraphics(connection);
+      var outline = gfx.select('.djs-outline');
+
+      expect(outline).toBeTruthy(); // OUTLINE EXISTS
+      expect(gfx.hasClass('selected')).toBeTruthy(); // Outline class is set
+    }));
+
+  });
+
+  describe('deselect', function() {
+
+    it('should remove outline class from shape', inject(function(selection, canvas, elementRegistry) {
+
+      // given
+      var shape = canvas.addShape({
+        id: 'test',
+        x: 10,
+        y: 10,
+        width: 100,
+        height: 100
+      });
+
+      // when
+      selection.select(shape);
+      selection.deselect(shape);
+
+      // then
+      var gfx = elementRegistry.getGraphics(shape);
+      var outline = gfx.select('.djs-outline');
+
+      expect(outline).toBeTruthy(); // OUTLINE box is not removed
+      expect(gfx.hasClass('selected')).toBeFalsy(); // Outline class is not set
+    }));
+
+    it('should remove outline class from connection', inject(function(selection, canvas, elementRegistry) {
+
+      // given
+      var connection = canvas.addConnection({ id: 'select3', waypoints: [ { x: 25, y: 25 }, {x: 115, y: 115} ]});
+
+      // when
+      selection.select(connection);
+      selection.deselect(connection);
+
+      // then
+      var gfx = elementRegistry.getGraphics(connection);
+      var outline = gfx.select('.djs-outline');
+
+      expect(outline).toBeTruthy(); // OUTLINE box is not removed
+      expect(gfx.hasClass('selected')).toBeFalsy(); // Outline class is not set
+    }));
+  });
+
+});
