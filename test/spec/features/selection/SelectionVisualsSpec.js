b1bb16459ffecad56aba9405c7874a0816141667
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2015-01-22 11:49:02 +0100

    test(selection): ensure connection selection works

--- test/spec/features/selection/SelectionVisualsSpec.js
+++ test/spec/features/selection/SelectionVisualsSpec.js
@@ -1,15 +1,16 @@
 'use strict';
 
-var TestHelper = require('../../../TestHelper');
+require('../../../TestHelper');
 
 /* global bootstrapDiagram, inject */
 
 
 var selectionModule = require('../../../../lib/features/selection');
-var overlayModule = require('../../../../lib/features/overlays');
 
 
 describe('features/selection/SelectionVisuals', function() {
+
+  beforeEach(bootstrapDiagram({ modules: [ selectionModule ] }));
 
   describe('bootstrap', function() {
 
@@ -20,4 +21,51 @@ describe('features/selection/SelectionVisuals', function() {
     }));
 
   });
+
+
+  ddescribe('selection box', function() {
+
+    var shape, shape2, connection;
+
+    beforeEach(inject(function(elementFactory, canvas) {
+
+      shape = elementFactory.createShape({
+        id: 'child',
+        x: 100, y: 100, width: 100, height: 100
+      });
+
+      canvas.addShape(shape);
+
+      shape2 = elementFactory.createShape({
+        id: 'child2',
+        x: 300, y: 100, width: 100, height: 100
+      });
+
+      canvas.addShape(shape2);
+
+      connection = elementFactory.createConnection({
+        id: 'connection',
+        waypoints: [ { x: 150, y: 150 }, { x: 150, y: 200 }, { x: 350, y: 150 } ],
+        source: shape,
+        target: shape2
+      });
+
+      canvas.addConnection(connection);
+    }));
+
+
+    it('should show box on select', inject(function(selection, canvas) {
+
+      // when
+      selection.select(connection);
+
+      // then
+      var gfx = canvas.getGraphics(connection),
+          outline = gfx.select('.djs-outline');
+
+      expect(outline).toBeDefined();
+    }));
+
+  });
+
 });
