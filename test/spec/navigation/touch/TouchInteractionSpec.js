b0d9bc9f1a5ccc1ae9a5f114ca6bcb881fdb99c3
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2015-02-06 09:38:55 +0100

    test(touch): verify correct contextPad+palette integration

--- test/spec/navigation/touch/TouchInteractionSpec.js
+++ test/spec/navigation/touch/TouchInteractionSpec.js
@@ -5,15 +5,58 @@ var TestHelper = require('../../../TestHelper');
 /* global bootstrapDiagram, inject */
 
 
-var touchModule = require('../../../../lib/navigation/touch');
+var touchModule = require('../../../../lib/navigation/touch'),
+    contextPadModule = require('../../../../lib/features/context-pad'),
+    paletteModule = require('../../../../lib/features/palette');
 
 
 describe('navigation/touch', function() {
 
-  beforeEach(bootstrapDiagram({ modules: [ touchModule ] }));
-
-
   describe('bootstrap', function() {
-    it('should bootstrap', inject(function() {}));
+
+    beforeEach(bootstrapDiagram({ modules: [ touchModule ] }));
+
+    it('should bootstrap', inject(function() { }));
   });
+
+
+  describe('integration', function() {
+
+    describe('contextPad', function() {
+
+      beforeEach(bootstrapDiagram({ modules: [ touchModule, contextPadModule ] }));
+
+      it('should integrate with contextPad.create', inject(function(canvas) {
+
+        canvas.addShape({
+          id: 'test',
+          width: 100,
+          height: 100,
+          x: 100,
+          y: 100
+        });
+      }));
+
+    });
+
+
+    describe('palette', function() {
+
+      beforeEach(bootstrapDiagram({ modules: [ touchModule, paletteModule ] }));
+
+      it('should integrate with palette.create', inject(function(canvas) {
+
+        canvas.addShape({
+          id: 'test',
+          width: 100,
+          height: 100,
+          x: 100,
+          y: 100
+        });
+
+      }));
+
+    });
+  });
+
 });
