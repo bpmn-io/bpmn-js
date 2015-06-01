0b0f5ac6c60e07c57f88219f08b82d5105a95629
Ricardo Matias <ricardo.matias@camunda.com>
2015-06-01 15:55:42 +0200

    feat(popup-menu): refactor + document
    collaborator: @pedesen
    
    Closes #88
    
    BREAKING CHANGE:
    
    We removed the ability to have multiple popup menues.

--- test/spec/features/popup-menu/PopupMenuSpec.js
+++ test/spec/features/popup-menu/PopupMenuSpec.js
@@ -2,8 +2,11 @@
 
 /* global bootstrapDiagram, inject */
 
-var popupMenuModule = require('../../../../lib/features/popup-menu');
+var domQuery = require('min-dom/lib/query');
 
+var Events = require('../../../util/Events');
+
+var popupMenuModule = require('../../../../lib/features/popup-menu');
 
 
 describe('features/popup', function() {
@@ -37,29 +40,6 @@ describe('features/popup', function() {
 
       // then
       expect(popup).to.be.defined;
-    }));
-
-    it('should return popup instance', inject(function(popupMenu) {
-
-      // when
-      var popup = popupMenu.open('popup-menu', {
-        x: 100,
-        y: 100
-      }, [
-        {label: 'Entry 1'},
-        {label: 'Entry 2'}
-      ]);
-
-      var popup2 = popupMenu.open('popup-menu', {
-        x: 200,
-        y: 200
-      }, [
-        {label: 'Entry 1'},
-        {label: 'Entry 2'}
-      ]);
-
-      // then
-      expect(popup).to.equal(popup2);
     }));
 
     it('should attach popup to html', inject(function(popupMenu) {
@@ -130,6 +110,7 @@ describe('features/popup', function() {
       expect(entry2.getAttribute('data-action')).to.equal('load');
       expect(entry3.getAttribute('data-action')).to.equal('undo');
     }));
+
   });
 
   describe('#close', function() {
@@ -159,6 +140,55 @@ describe('features/popup', function() {
 
       expect(removed).to.be.true;
     }));
+
+  });
+
+  describe('#trigger', function () {
+
+    beforeEach(bootstrapDiagram({ modules: [ popupMenuModule ] }));
+
+    it('should trigger the right action handler', inject(function(popupMenu) {
+
+      var entry, evt, trigger;
+
+      // given
+      var popup = popupMenu.open('popup-menu', {
+        x: 100,
+        y: 100
+      }, [
+      {
+          label: 'Entry 1',
+          className: 'Entry_1',
+          action: {
+            name: 'button 1',
+            handler: function() {
+              return 'Entry 1';
+            }
+          }
+      }, {
+          label: 'Entry 2',
+          className: 'Entry_2',
+          action: {
+            name: 'button 2',
+            handler: function() {
+              return 'Entry 2';
+            }
+          }
+      }], {
+        className: 'test-popup'
+      });
+
+      entry = domQuery('.Entry_2', popup._container);
+
+      evt = Events.create(entry, { x: 0, y: 0 });
+
+      // when
+      trigger = popup.trigger(evt);
+
+      // then
+      expect(trigger).to.equal('Entry 2');
+    }));
+
   });
 
 
@@ -215,7 +245,9 @@ describe('features/popup', function() {
         var removed = popup._container.parentNode === null;
         expect(removed).to.be.true;
       }));
+
     });
+
   });
 
 
@@ -275,5 +307,7 @@ describe('features/popup', function() {
       var element = popup._container.querySelector('.special-entry');
       expect(element.getAttribute('style')).to.equal('color: rgb(222,67,157)');
     }));
+
   });
+
 });
