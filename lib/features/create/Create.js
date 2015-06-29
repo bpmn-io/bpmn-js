theturtle32/diagram-js

feat(attaching): elements can be attached to other elements
This commit adds an attachment concept. It resides on a one to many relationship
between a host and an attacher. A host can have many attachers, but an attacher
can only have one host.

Like with the parent/child relationship, when the host moves the attachers move with it.

closes #87

lib/features/create/Create.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var MARKER_OK = 'drop-ok',
-    MARKER_NOT_OK = 'drop-not-ok';
+    MARKER_NOT_OK = 'drop-not-ok',
+    MARKER_ATTACH = 'attach-ok';
 
 
-function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, renderer, styles) {
+function Create(eventBus, dragging, rules, modeling, canvas, renderer, styles) {
 
   // rules
 
@@ -14,12 +15,12 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
       return rules.allowed('shape.append', {
         source: source,
         shape: shape,
-        parent: target
+        target: target
       });
     } else {
       return rules.allowed('shape.create', {
         shape: shape,
-        parent: target
+        target: target
       });
     }
   }
@@ -69,7 +70,14 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
     // ignore hover visually if canExecute is null
     if (hover && canExecute !== null) {
       context.target = hover;
-      canvas.addMarker(hover, canExecute ? MARKER_OK : MARKER_NOT_OK);
+
+      switch (canExecute) {
+        case 'attach':
+          canvas.addMarker(hover, MARKER_ATTACH);
+          break;
+        default:
+          canvas.addMarker(hover, canExecute ? MARKER_OK : MARKER_NOT_OK);
+      }
     }
   });
 
@@ -77,7 +85,13 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
     var context = event.context;
 
     if (context.target) {
-      canvas.removeMarker(context.target, context.canExecute ? MARKER_OK : MARKER_NOT_OK);
+      switch (context.canExecute) {
+        case 'attach':
+          canvas.removeMarker(context.target, MARKER_ATTACH);
+          break;
+        default:
+          canvas.removeMarker(context.target, context.canExecute ? MARKER_OK : MARKER_NOT_OK);
+      }
     }
   });
 
@@ -90,7 +104,8 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
         position = {
           x: event.x,
           y: event.y
-        };
+        },
+        isAttach;
 
     if (!canExecute) {
       return false;
@@ -99,13 +114,16 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
     if (source) {
       modeling.appendShape(source, shape, position, target);
     } else {
-      modeling.createShape(shape, position, target);
+      isAttach = canExecute === 'attach';
+
+      modeling.createShape(shape, position, target, isAttach);
     }
   });
 
 
   eventBus.on('create.cleanup', function(event) {
     var context = event.context;
+
 
     if (context.visual) {
       context.visual.remove();
@@ -130,6 +148,6 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
   };
 }
 
-Create.$inject = [ 'eventBus', 'dragging', 'rules', 'modeling', 'canvas', 'elementFactory', 'renderer', 'styles' ];
+Create.$inject = [ 'eventBus', 'dragging', 'rules', 'modeling', 'canvas', 'renderer', 'styles' ];
 
 module.exports = Create;

lib/features/modeling/cmd/MoveShapesHandler.js
@@ -19,7 +19,7 @@ MoveShapesHandler.prototype.preExecute = function(context) {
 };
 
 MoveShapesHandler.prototype.postExecute = function(context) {
-  this._helper.moveClosure(context.closure, context.delta, context.newParent);
+  this._helper.moveClosure(context.closure, context.delta, context.newParent, context.isAttach);
 };
 
 

bpmn-io/bpmn-js

lib/features/create/Create.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var MARKER_OK = 'drop-ok',
-    MARKER_NOT_OK = 'drop-not-ok';
+    MARKER_NOT_OK = 'drop-not-ok',
+    MARKER_ATTACH = 'attach-ok';
 
 
-function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, renderer, styles) {
+function Create(eventBus, dragging, rules, modeling, canvas, renderer, styles) {
 
   // rules
 
@@ -14,12 +15,12 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
       return rules.allowed('shape.append', {
         source: source,
         shape: shape,
-        parent: target
+        target: target
       });
     } else {
       return rules.allowed('shape.create', {
         shape: shape,
-        parent: target
+        target: target
       });
     }
   }
@@ -69,7 +70,14 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
     // ignore hover visually if canExecute is null
     if (hover && canExecute !== null) {
       context.target = hover;
-      canvas.addMarker(hover, canExecute ? MARKER_OK : MARKER_NOT_OK);
+
+      switch (canExecute) {
+        case 'attach':
+          canvas.addMarker(hover, MARKER_ATTACH);
+          break;
+        default:
+          canvas.addMarker(hover, canExecute ? MARKER_OK : MARKER_NOT_OK);
+      }
     }
   });
 
@@ -77,7 +85,13 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
     var context = event.context;
 
     if (context.target) {
-      canvas.removeMarker(context.target, context.canExecute ? MARKER_OK : MARKER_NOT_OK);
+      switch (context.canExecute) {
+        case 'attach':
+          canvas.removeMarker(context.target, MARKER_ATTACH);
+          break;
+        default:
+          canvas.removeMarker(context.target, context.canExecute ? MARKER_OK : MARKER_NOT_OK);
+      }
     }
   });
 
@@ -90,7 +104,8 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
         position = {
           x: event.x,
           y: event.y
-        };
+        },
+        isAttachment;
 
     if (!canExecute) {
       return false;
@@ -99,13 +114,16 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
     if (source) {
       modeling.appendShape(source, shape, position, target);
     } else {
-      modeling.createShape(shape, position, target);
+      isAttachment = canExecute === 'attach';
+
+      modeling.createShape(shape, position, target, isAttachment);
     }
   });
 
 
   eventBus.on('create.cleanup', function(event) {
     var context = event.context;
+
 
     if (context.visual) {
       context.visual.remove();
@@ -130,6 +148,6 @@ function Create(eventBus, dragging, rules, modeling, canvas, elementFactory, ren
   };
 }
 
-Create.$inject = [ 'eventBus', 'dragging', 'rules', 'modeling', 'canvas', 'elementFactory', 'renderer', 'styles' ];
+Create.$inject = [ 'eventBus', 'dragging', 'rules', 'modeling', 'canvas', 'renderer', 'styles' ];
 
 module.exports = Create;

lib/features/modeling/cmd/MoveShapesHandler.js
@@ -19,7 +19,7 @@ MoveShapesHandler.prototype.preExecute = function(context) {
 };
 
 MoveShapesHandler.prototype.postExecute = function(context) {
-  this._helper.moveClosure(context.closure, context.delta, context.newParent);
+  this._helper.moveClosure(context.closure, context.delta, context.newParent, context.isAttachment);
 };
 
 