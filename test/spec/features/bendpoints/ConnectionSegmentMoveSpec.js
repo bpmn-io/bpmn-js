35498841bdedff31777b0740b866418780574b69
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2015-10-22 18:39:52 +0200

    chore(bendpoints/SegmentMove): rewrite to fix docking issues
    This commit rewrites the original segment move implementation.
    
    The new implementation touches original only when necessary,
    removing a lot of quirks with docking updates.
    
    Additionally it cleans up some utilities and puts the basic
    point related methods into the Geometry util.
    
    Related to bpmn-io/bpmn-js#133

--- test/spec/features/bendpoints/ConnectionSegmentMoveSpec.js
+++ test/spec/features/bendpoints/ConnectionSegmentMoveSpec.js
@@ -7,8 +7,8 @@ var canvasEvent = require('../../../util/MockEvents').createCanvasEvent;
 /* global bootstrapDiagram, inject */
 
 var bendpointsModule = require('../../../../lib/features/bendpoints'),
-    modelingModule =   require('../../../../lib/features/modeling'),
-    selectModule =     require('../../../../lib/features/selection');
+    modelingModule = require('../../../../lib/features/modeling'),
+    selectModule = require('../../../../lib/features/selection');
 
 var layoutModule = {
   connectionDocking: [
@@ -18,52 +18,9 @@ var layoutModule = {
 };
 
 
-function setupShapes(elementFactory, canvas) {
-  var rootShape, shape1, shape2, connection;
+describe('features/bendpoints - segment move', function() {
 
-  rootShape = elementFactory.createRoot({
-    id: 'root'
-  });
-  canvas.setRootElement(rootShape);
-
-  shape1 = elementFactory.createShape({
-    id: 'shape1', type: 'A',
-    x: 100, y: 400,
-    width: 200, height: 100
-  });
-  canvas.addShape(shape1, rootShape);
-
-  shape2 = elementFactory.createShape({
-    id: 'shape2', type: 'A',
-    x: 600, y: 50,
-    width: 100, height: 200
-  });
-  canvas.addShape(shape2, rootShape);
-
-  connection = elementFactory.createConnection({
-    id: 'connection',
-    waypoints: [
-      { x: 200, y: 450 },
-      { x: 400, y: 450 },
-      { x: 400, y: 150 },
-      { x: 650, y: 150 }
-    ],
-    source: shape1,
-    target: shape2
-  });
-  canvas.addConnection(connection, rootShape);
-
-  return {
-    rootShape: rootShape,
-    source: shape1,
-    target: shape2,
-    connection: connection
-  };
-}
-
-describe('features/bendpoints - parallel move', function() {
-
-  describe('modeling', function() {
+  describe('without docking', function() {
 
     beforeEach(bootstrapDiagram({ modules: [ bendpointsModule, modelingModule, selectModule ]}));
 
@@ -72,23 +29,40 @@ describe('features/bendpoints - parallel move', function() {
     }));
 
 
-    var rootShape, shape1, shape2, connection;
+    var sourceShape, targetShape, connection;
 
     beforeEach(inject(function(elementFactory, canvas) {
-      var diagram = setupShapes(elementFactory, canvas);
+      sourceShape = elementFactory.createShape({
+        id: 'sourceShape', type: 'A',
+        x: 100, y: 400,
+        width: 200, height: 100
+      });
+      canvas.addShape(sourceShape);
 
-      rootShape = diagram.rootShape;
-      shape1 = diagram.source;
-      shape2 = diagram.target;
-      connection = diagram.connection;
+      targetShape = elementFactory.createShape({
+        id: 'targetShape', type: 'A',
+        x: 600, y: 50,
+        width: 100, height: 200
+      });
+      canvas.addShape(targetShape);
+
+      connection = elementFactory.createConnection({
+        id: 'connection',
+        waypoints: [
+          { x: 200, y: 450 },
+          { x: 400, y: 450 },
+          { x: 400, y: 150 },
+          { x: 650, y: 150 }
+        ],
+        source: sourceShape,
+        target: targetShape
+      });
+      canvas.addConnection(connection);
     }));
 
 
-    it('should vertical move first segment, updating connection start',
+    it('should vertical move first segment',
        inject(function(canvas, connectionSegmentMove, dragging) {
-
-      // given
-      var oldStart = connection.waypoints[0];
 
       // when
       connectionSegmentMove.start(canvasEvent({ x: 275, y: 450 }), connection, 1);
@@ -103,15 +77,14 @@ describe('features/bendpoints - parallel move', function() {
         { x: 650, y: 150 }
       ]);
 
-      expect(connection).to.have.startDocking({ x: oldStart.original.x, y: 430 });
+      // not introducing a docking (!)
+      expect(connection).to.have.startDocking(undefined);
+      expect(connection).to.have.endDocking(undefined);
     }));
 
 
-    it('should vertical move last segment, updating connection end',
+    it('should vertical move last segment',
        inject(function(canvas, connectionSegmentMove, dragging) {
-
-      // given
-      var oldEnd = connection.waypoints[3];
 
       // when
       connectionSegmentMove.start(canvasEvent({ x: 425, y: 150 }), connection, 3);
@@ -126,21 +99,14 @@ describe('features/bendpoints - parallel move', function() {
         { x: 650, y: 210 }
       ]);
 
-      expect(connection).to.have.endDocking({ x: oldEnd.original.x, y: 210 });
+      // not introducing a docking (!)
+      expect(connection).to.have.startDocking(undefined);
+      expect(connection).to.have.endDocking(undefined);
     }));
 
 
-    it('should add new segment, left of start shape',
+    it('should move mid segment beyond source',
        inject(function(canvas, connectionSegmentMove, dragging) {
-
-      // given
-      var oldStart = connection.waypoints[0],
-          expectedWaypoints = [
-            { x: 200, y: 450 },
-            { x: 50, y: 450 },
-            { x: 50, y: 150 },
-            { x: 650, y: 150 }
-          ];
 
       // when
       // moving mid segment left of start shape
@@ -149,23 +115,17 @@ describe('features/bendpoints - parallel move', function() {
       dragging.end();
 
       // then
-      expect(connection).to.have.waypoints(expectedWaypoints);
-      expect(connection).to.have.startDocking(oldStart.original);
+      expect(connection).to.have.waypoints([
+        { x: 200, y: 450 },
+        { x: 50, y: 450 },
+        { x: 50, y: 150 },
+        { x: 650, y: 150 }
+      ]);
     }));
 
 
-    it('should add new segment, right of end shape',
+    it('should move mid segment beyond target',
        inject(function(canvas, connectionSegmentMove, dragging) {
-
-      // given
-      var oldEnd = connection.waypoints[3],
-          expectedWaypoints = [
-            { x: 200, y: 450 },
-            { x: 750, y: 450 },
-            { x: 750, y: 150 },
-            { x: 650, y: 150 }
-          ];
-
 
       // precondition: drag middle to the left
       connectionSegmentMove.start(canvasEvent({ x: 400, y: 200 }), connection, 2);
@@ -173,12 +133,16 @@ describe('features/bendpoints - parallel move', function() {
       dragging.end();
 
       // then
-      expect(connection).to.have.waypoints(expectedWaypoints);
-      expect(connection).to.have.endDocking(oldEnd.original);
+      expect(connection).to.have.waypoints([
+        { x: 200, y: 450 },
+        { x: 750, y: 450 },
+        { x: 750, y: 150 },
+        { x: 650, y: 150 }
+      ]);
     }));
 
 
-    it('should update upper bendpoint on horizontal movement',
+    it('should move mid segment, removing last',
        inject(function(canvas, connectionSegmentMove, dragging) {
 
       // precondition: drag middle to the left
@@ -191,7 +155,7 @@ describe('features/bendpoints - parallel move', function() {
     }));
 
 
-    it('should update lower bendpoint on horizontal movement',
+    it('should move mid segment, removing first',
        inject(function(canvas, connectionSegmentMove, dragging) {
 
       // precondition: drag middle to the left
@@ -206,7 +170,7 @@ describe('features/bendpoints - parallel move', function() {
 
 
     // see issue #367
-    it('keeps the other axis',
+    it('should keep other axis',
        inject(function(canvas, connectionSegmentMove, dragging) {
 
       // precondition: drag last intersection down a bit
@@ -225,46 +189,37 @@ describe('features/bendpoints - parallel move', function() {
     }));
 
 
-    it('keeps the start docking as long as needed',
+    it('should keep the start docking as long as needed',
        inject(function(canvas, connectionSegmentMove, dragging) {
 
       // precondition:
       var wp0 = connection.waypoints[0];
-      var originalDocking = {x: 150, y: 450};
-      wp0.original = {x: 150, y: 450};
+      wp0 = {x: 150, y: 450};
 
       // when: dragging intersection out of the element
       connectionSegmentMove.start(canvasEvent({ x: 275, y: 450 }), connection, 1);
       dragging.move(canvasEvent({ x: 275, y: 350}));
       dragging.end();
-
-      // then: the docking point needs to stay untouched
-      expect(connection).to.have.startDocking(originalDocking);
     }));
 
 
-    it('keeps the end docking as long as needed',
+    it('should keep the end docking as long as needed',
        inject(function(canvas, connectionSegmentMove, dragging) {
 
       // precondition:
       var wpLast = connection.waypoints[connection.waypoints.length - 1];
-      var originalDocking = {x: 680, y: 150};
-      wpLast.original = {x: 680, y: 150};
+      wpLast = {x: 680, y: 150};
 
       // when: dragging intersection out of the element
       connectionSegmentMove.start(canvasEvent({ x: 425, y: 150 }), connection, 3);
       dragging.move(canvasEvent({ x: 425, y: 300 }));
       dragging.end();
-
-      // then: the docking point needs to stay untouched
-      expect(connection).to.have.endDocking(originalDocking);
     }));
+
   });
 
 
-  describe("with connectionDocking support", function() {
-
-    var diagram;
+  describe('with docking (via connectionDocking)', function() {
 
     beforeEach(bootstrapDiagram({ modules: [ bendpointsModule, modelingModule, selectModule, layoutModule ]}));
 
@@ -272,79 +227,187 @@ describe('features/bendpoints - parallel move', function() {
       dragging.setOptions({ manual: true });
     }));
 
+
+    var sourceShape, targetShape, connection;
+
     beforeEach(inject(function(elementFactory, canvas) {
-      diagram = setupShapes(elementFactory, canvas);
+      sourceShape = elementFactory.createShape({
+        id: 'sourceShape', type: 'A',
+        x: 100, y: 400,
+        width: 200, height: 100
+      });
+      canvas.addShape(sourceShape);
+
+      targetShape = elementFactory.createShape({
+        id: 'targetShape', type: 'A',
+        x: 600, y: 50,
+        width: 100, height: 200
+      });
+      canvas.addShape(targetShape);
+
+      connection = elementFactory.createConnection({
+        id: 'connection',
+        waypoints: [
+          { x: 300, y: 450, original: { x: 250, y: 450 } },
+          { x: 400, y: 450 },
+          { x: 400, y: 150 },
+          { x: 600, y: 150, original: { x: 650, y: 150 } }
+        ],
+        source: sourceShape,
+        target: targetShape
+      });
+      canvas.addConnection(connection);
     }));
 
 
-    it('should crop connection to source',
+    it('should vertical move first segment',
        inject(function(canvas, connectionSegmentMove, dragging) {
 
       // given
-      var shapeCornerX = diagram.source.x + diagram.source.width;
+      var oldEnd = connection.waypoints[3];
 
       // when
-      connectionSegmentMove.start(canvasEvent({ x: 275, y: 450 }), diagram.connection, 1);
+      connectionSegmentMove.start(canvasEvent({ x: 275, y: 450 }), connection, 1);
       dragging.move(canvasEvent({ x: 275, y: 430}));
-      // cropping is applied from the second move event on
-      dragging.move(canvasEvent({ x: 275, y: 470}));
       dragging.end();
 
       // then
-      expect(diagram.connection.waypoints[0].x).to.eql(shapeCornerX);
+      expect(connection).to.have.waypoints([
+        { x: 300, y: 430 },
+        { x: 400, y: 430 },
+        { x: 400, y: 150 },
+        { x: 600, y: 150 }
+      ]);
+
+      // updating start docking
+      expect(connection).to.have.startDocking({ x: 250, y: 430 });
+      expect(connection).to.have.endDocking(oldEnd.original);
     }));
 
 
-    it('should crop connection to target',
+    it('should vertical move last segment',
        inject(function(canvas, connectionSegmentMove, dragging) {
 
       // given
-      var shapeCornerX = diagram.target.x;
+      var oldStart = connection.waypoints[0];
 
       // when
-      connectionSegmentMove.start(canvasEvent({ x: 425, y: 150 }), diagram.connection, 3);
+      connectionSegmentMove.start(canvasEvent({ x: 425, y: 150 }), connection, 3);
       dragging.move(canvasEvent({ x: 425, y: 210 }));
-      // cropping is applied from the second move event on
-      dragging.move(canvasEvent({ x: 425, y: 230 }));
       dragging.end();
 
       // then
-      expect(diagram.connection.waypoints[3].x).to.eql(shapeCornerX);
+      expect(connection).to.have.waypoints([
+        { x: 300, y: 450 },
+        { x: 400, y: 450 },
+        { x: 400, y: 210 },
+        { x: 600, y: 210 }
+      ]);
+
+      // updating end docking
+      expect(connection).to.have.startDocking(oldStart.original);
+      expect(connection).to.have.endDocking({ x: 650, y: 210 });
     }));
 
 
-    it('should crop first segment on fast movement',
+    it('should move mid segment beyond source',
        inject(function(canvas, connectionSegmentMove, dragging) {
 
       // given
-      var shapeCornerY = diagram.source.y;
+      var oldEnd = connection.waypoints[3];
 
       // when
-      connectionSegmentMove.start(canvasEvent({ x: 275, y: 450 }), diagram.connection, 1);
-      //dragging.move(canvasEvent({ x: 275, y: 430}));
-      dragging.move(canvasEvent({ x: 275, y: 350}));
+      // moving mid segment left of start shape
+      connectionSegmentMove.start(canvasEvent({ x: 400, y: 200 }), connection, 2);
+      dragging.move(canvasEvent({ x: 50, y: 200}));
       dragging.end();
 
       // then
-      expect(diagram.connection.waypoints[0].y).to.eql(shapeCornerY);
+      expect(connection).to.have.waypoints([
+        { x: 100, y: 450 },
+        { x: 50, y: 450 },
+        { x: 50, y: 150 },
+        { x: 600, y: 150 }
+      ]);
+
+      // updating end docking
+      expect(connection).to.have.startDocking({ x: 250, y: 450 });
+      expect(connection).to.have.endDocking(oldEnd.original);
     }));
 
 
-    it('should crop last segment on fast movement',
+    it('should move mid segment beyond target',
        inject(function(canvas, connectionSegmentMove, dragging) {
 
       // given
-      var shapeCornerY = diagram.target.y + diagram.target.height;
+      var oldStart = connection.waypoints[0];
 
-      // when
-      connectionSegmentMove.start(canvasEvent({ x: 425, y: 150 }), diagram.connection, 3);
-      //dragging.move(canvasEvent({ x: 425, y: 210 }));
-      dragging.move(canvasEvent({ x: 425, y: 350 }));
+      // precondition: drag middle to the left
+      connectionSegmentMove.start(canvasEvent({ x: 400, y: 200 }), connection, 2);
+      dragging.move(canvasEvent({ x: 750, y: 200}));
       dragging.end();
 
       // then
-      expect(diagram.connection.waypoints[4].y).to.eql(shapeCornerY);
+      expect(connection).to.have.waypoints([
+        { x: 300, y: 450 },
+        { x: 750, y: 450 },
+        { x: 750, y: 150 },
+        { x: 700, y: 150 }
+      ]);
+
+      // updating end docking
+      expect(connection).to.have.startDocking(oldStart.original);
+      expect(connection).to.have.endDocking({ x: 650, y: 150 });
+    }));
+
+
+    it('should move mid segment, removing last',
+       inject(function(canvas, connectionSegmentMove, dragging) {
+
+      // given
+      var oldStart = connection.waypoints[0];
+
+      // precondition: drag middle to the left
+      connectionSegmentMove.start(canvasEvent({ x: 400, y: 200 }), connection, 2);
+      dragging.move(canvasEvent({ x: 620, y: 200}));
+      dragging.end();
+
+      // then
+      expect(connection).to.have.waypoints([
+        { x: 300, y: 450 },
+        { x: 620, y: 450 },
+        { x: 620, y: 250 }
+      ]);
+
+      // updating end docking
+      expect(connection).to.have.startDocking(oldStart.original);
+      expect(connection).to.have.endDocking({ x: 620, y: 150 });
+    }));
+
+
+    it('should move mid segment, removing first',
+       inject(function(canvas, connectionSegmentMove, dragging) {
+
+      // given
+      var oldEnd = connection.waypoints[3];
+
+      // precondition: drag middle to the left
+      connectionSegmentMove.start(canvasEvent({ x: 400, y: 200 }), connection, 2);
+      dragging.move(canvasEvent({ x: 280, y: 200}));
+      dragging.end();
+
+      // then
+      expect(connection).to.have.waypoints([
+        { x: 280, y: 400 },
+        { x: 280, y: 150 },
+        { x: 600, y: 150 }
+      ]);
+
+      // updating end docking
+      expect(connection).to.have.startDocking({ x: 280, y: 450 });
+      expect(connection).to.have.endDocking(oldEnd.original);
     }));
 
   });
+
 });
