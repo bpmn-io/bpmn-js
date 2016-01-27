206daa11549765d0fdb36627debdbda4cfbcf647
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2016-01-27 09:45:05 +0100

    fix(modeling): manhattan layout compensation association
    Related to #291

---
+++ test/spec/features/modeling/behavior/CompensationAssociationBehaviorSpec.js
@@ -0,0 +1,46 @@
+'use strict';
+
+require('../../../../TestHelper');
+
+/* global inject, bootstrapModeler */
+
+var find = require('lodash/collection/find');
+
+var modelingModule = require('../../../../../lib/features/modeling');
+
+
+describe('modeling/behavior - CompensationAssociation', function(){
+
+  var diagramXML = require('./CompensationAssociationBehavior.bpmn');
+
+  beforeEach(bootstrapModeler(diagramXML, { modules: modelingModule }));
+
+
+  it('should manhattan layout', inject(function(modeling, elementRegistry) {
+
+    // given
+    var boundaryShape = elementRegistry.get('CompensationBoundary'),
+        activityShape = elementRegistry.get('CompensationActivity');
+
+
+    // when
+    var newConnection = modeling.connect(boundaryShape, activityShape, {
+      type: 'bpmn:DataInputAssociation'
+    });
+
+    // then
+    expect(waypoints(newConnection)).to.eql([
+      { x: 107, y: 142 },
+      { x: 107, y: 254 },
+      { x: 206, y: 254 }
+    ]);
+  }));
+
+});
+
+
+function waypoints(connection) {
+  return connection.waypoints.map(function(wp) {
+    return { x: wp.x, y: wp.y };
+  });
+}
