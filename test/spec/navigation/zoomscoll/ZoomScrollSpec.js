1fe8f099d9daae5823a07f0896cbba5d9a873025
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2016-01-28 09:00:19 +0100

    feat(navigation/zoomscroll): allow toggle via API
    It is now possible to temorarily disable the mouse wheel
    interaction in zoomScroll via ZoomScroll#toggle(enabled).
    
    Additionaly users can control the initial state via
    the zoomScroll.enabled setting.
    
    Closes #67

--- test/spec/navigation/zoomscoll/ZoomScrollSpec.js
+++ test/spec/navigation/zoomscoll/ZoomScrollSpec.js
@@ -10,10 +10,13 @@ var zoomScrollModule = require('../../../../lib/navigation/zoomscroll');
 
 describe('navigation/zoomscroll', function() {
 
-  beforeEach(bootstrapDiagram({ modules: [ zoomScrollModule ], canvas: { deferUpdate: false } }));
-
-
   describe('bootstrap', function() {
+
+    beforeEach(bootstrapDiagram({
+      modules: [ zoomScrollModule ],
+      canvas: { deferUpdate: false }
+    }));
+
 
     it('should bootstrap', inject(function(zoomScroll, canvas) {
 
@@ -25,12 +28,20 @@ describe('navigation/zoomscroll', function() {
         y: 100
       });
 
-      expect(zoomScroll).to.not.be.null;
+      expect(zoomScroll).to.exist;
+      expect(zoomScroll._enabled).to.be.true;
     }));
 
   });
 
-  describe('step zoom', function(){
+
+  describe('step zoom', function() {
+
+    beforeEach(bootstrapDiagram({
+      modules: [ zoomScrollModule ],
+      canvas: { deferUpdate: false }
+    }));
+
 
     it('should zoom in along fixed zoom steps', inject(function(zoomScroll, canvas){
 
@@ -85,7 +96,7 @@ describe('navigation/zoomscroll', function() {
 
 
     it('should snap to default zoom level (1.00) when zooming out from the maximum zoom level',
-      inject(function(zoomScroll, canvas){
+      inject(function(zoomScroll, canvas) {
 
       // given
       // zoom to the maximum level
@@ -99,6 +110,75 @@ describe('navigation/zoomscroll', function() {
 
       // then
       expect(canvas.zoom()).to.equal(1);
+    }));
+
+  });
+
+
+  describe('toggle', function() {
+
+    beforeEach(bootstrapDiagram({
+      modules: [ zoomScrollModule ],
+      canvas: { deferUpdate: false },
+      zoomScroll: {
+        enabled: false
+      }
+    }));
+
+
+    it('should force enable', inject(function(zoomScroll) {
+      // when
+      var newEnabled = zoomScroll.toggle(true);
+
+      // then
+      expect(newEnabled).to.be.true;
+    }));
+
+
+    it('should force disable', inject(function(zoomScroll) {
+      // when
+      var newEnabled = zoomScroll.toggle(false);
+
+      // then
+      expect(newEnabled).to.be.false;
+    }));
+
+
+    it('should toggle on', inject(function(zoomScroll) {
+      // assume
+      expect(zoomScroll._enabled).to.be.false;
+
+      // when
+      var newEnabled = zoomScroll.toggle();
+
+      // then
+      expect(newEnabled).to.be.true;
+    }));
+
+
+    it('should toggle enabled state on diagram click',
+        inject(function(canvas, elementRegistry, zoomScroll) {
+
+      // given
+      canvas.addShape({
+        id: 'test',
+        width: 100,
+        height: 100,
+        x: 100,
+        y: 100
+      });
+
+      var rootNode = canvas.getContainer();
+
+      // when
+      rootNode.addEventListener('click', function() {
+        var newState = zoomScroll.toggle();
+
+        rootNode.style.background = newState ? 'lightgreen' : 'none';
+      });
+
+      // then
+      // test manually if zoom scroll is actually toggled...
     }));
 
   });
