0fb1bf616ec3b06e706dd260dc0466bb92990493
davcs86 <davcs86@gmail.com>
2016-02-17 18:16:10 -0600

    feat: add additional (default!) moddle descriptor to viewer
    Closes #471

--- test/spec/ViewerSpec.js
+++ test/spec/ViewerSpec.js
@@ -545,6 +545,54 @@ describe('Viewer', function() {
     });
 
 
+    it.only('should allow to add default custom moddle extensions', function(done) {
+
+      var xml = require('../fixtures/bpmn/extension/custom.bpmn'),
+          additionalModdleDescriptors = {
+            custom: require('../fixtures/json/model/custom')
+          };
+
+      Viewer.prototype._moddleExtensions = additionalModdleDescriptors;
+
+      // given
+      viewer = new Viewer({
+        container: container,
+        moddleExtensions: {
+          camunda: camundaPackage
+        }
+      });
+
+      // when
+      viewer.importXML(xml, function(err, warnings) {
+
+        var elementRegistry = viewer.get('elementRegistry');
+
+        var taskShape = elementRegistry.get('send'),
+            sendTask = taskShape.businessObject;
+
+        // then
+        expect(sendTask).to.exist;
+
+        var extensionElements = sendTask.extensionElements;
+
+        // receive task should be moddle extended
+        expect(sendTask.$instanceOf('camunda:ServiceTaskLike')).to.exist;
+        expect(sendTask.$instanceOf('custom:ServiceTaskGroup')).to.exist;
+
+        // extension elements should provide typed element
+        expect(extensionElements).to.exist;
+
+        expect(extensionElements.values.length).to.equal(2);
+        expect(extensionElements.values[0].$instanceOf('camunda:InputOutput')).to.exist;
+
+        expect(extensionElements.values[1].$instanceOf('custom:CustomSendElement')).to.exist;
+
+        done(err);
+      });
+
+    });
+
+
     it('should throw error due to missing diagram', function(done) {
 
       var xml = require('../fixtures/bpmn/empty-definitions.bpmn');
