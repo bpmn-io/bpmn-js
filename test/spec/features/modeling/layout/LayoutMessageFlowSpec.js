fcf2bbd9aab3c79e1e9116f3d36ce0739fffa841
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2016-06-22 14:36:44 +0200

    feat(modeling/layout): improve MessageFlow layout on SubProcesses
    Related to #574

--- test/spec/features/modeling/layout/LayoutMessageFlowSpec.js
+++ test/spec/features/modeling/layout/LayoutMessageFlowSpec.js
@@ -21,7 +21,7 @@ describe('features/modeling - layout message flows', function() {
   it('should layout manhattan after Task move', inject(function(elementRegistry, modeling) {
 
     // given
-    var taskShape = elementRegistry.get('Task_1'),
+    var taskShape = elementRegistry.get('Task_A'),
         messageFlowConnection = elementRegistry.get('MessageFlow_4');
 
     // when
@@ -29,7 +29,7 @@ describe('features/modeling - layout message flows', function() {
 
     // then
     // expect cropped, repaired manhattan connection
-    expect(messageFlowConnection.waypoints).eql([
+    expect(messageFlowConnection).to.have.waypoints([
       { original: { x: 420, y: 234 }, x: 420, y: 234 },
       { x: 420, y: 387 },
       { x: 318, y: 387 },
@@ -38,29 +38,31 @@ describe('features/modeling - layout message flows', function() {
   }));
 
 
-  it('should layout straight after Task move', inject(function(elementRegistry, modeling) {
+  it('should layout Task -> Participant straight after Task move',
+    inject(function(elementRegistry, modeling) {
 
-    // given
-    var taskShape = elementRegistry.get('Task_2'),
-        messageFlowConnection = elementRegistry.get('MessageFlow_1');
+      // given
+      var taskShape = elementRegistry.get('Task_B'),
+          messageFlowConnection = elementRegistry.get('MessageFlow_1');
 
-    // when
-    modeling.moveElements([ taskShape ], { x: 20, y: -20 });
+      // when
+      modeling.moveElements([ taskShape ], { x: 20, y: -20 });
 
-    // then
+      // then
 
-    // expect cropped, repaired manhattan connection
-    expect(messageFlowConnection.waypoints).eql([
-      { original: { x: 610, y: 194 }, x: 610, y: 194 },
-      { original: { x: 610, y: 415 }, x: 610, y: 415 }
-    ]);
-  }));
+      // expect cropped, repaired manhattan connection
+      expect(messageFlowConnection).to.have.waypoints([
+        { original: { x: 610, y: 194 }, x: 610, y: 194 },
+        { original: { x: 610, y: 415 }, x: 610, y: 415 }
+      ]);
+    })
+  );
 
 
   it('should layout straight after Participant move', inject(function(elementRegistry, modeling) {
 
     // given
-    var participantShape = elementRegistry.get('Participant_1'),
+    var participantShape = elementRegistry.get('Participant_B'),
         messageFlowConnection = elementRegistry.get('MessageFlow_5');
 
     // when
@@ -69,52 +71,26 @@ describe('features/modeling - layout message flows', function() {
     // then
 
     // expect cropped, repaired manhattan connection
-    expect(messageFlowConnection.waypoints).eql([
+    expect(messageFlowConnection).to.have.waypoints([
       { original: { x: 671, y: 214 }, x: 671, y: 214 },
       { original: { x: 671, y: 465 }, x: 671, y: 465 }
     ]);
   }));
 
 
-  it('should layout straight after Participant move, without moving Task\'s docking point',
-  inject(function(elementRegistry, modeling) {
-
-    // given
-    var participantShape = elementRegistry.get('Participant_1'),
-        messageFlow1 = elementRegistry.get('MessageFlow_1'),
-        messageFlow6 = elementRegistry.get('MessageFlow_6');
-
-    // when
-    modeling.moveElements([ participantShape ], { x: 300, y: 50 });
-    // then
-
-    // expect cropped, repaired manhattan connection
-    expect(messageFlow1.waypoints).eql([
-      { original: { x: 590, y: 214 }, x: 590, y: 214 },
-      { original: { x: 590, y: 465 }, x: 590, y: 465 }
-    ]);
-
-    expect(messageFlow6.waypoints).eql([
-      { original: { x: 773, y: 465 }, x: 773, y: 465 },
-      { original: { x: 773, y: 214 }, x: 773, y: 214 }
-    ]);
-  }));
-
-
-  it('should layout manhattan after Participant move beyond EndEvent bounds',
+  it('should layout EndEvent -> Participant manhattan',
     inject(function(elementRegistry, modeling) {
 
       // given
-      var participantShape = elementRegistry.get('Participant_1'),
+      var participantShape = elementRegistry.get('Participant_B'),
           messageFlowConnection = elementRegistry.get('MessageFlow_5');
 
       // when
       modeling.moveElements([ participantShape ], { x: -200, y: 0 });
 
       // then
-
       // expect cropped, repaired manhattan connection
-      expect(messageFlowConnection.waypoints).eql([
+      expect(messageFlowConnection).to.have.waypoints([
         { original: { x: 671, y: 214 }, x: 671, y: 214 },
         { x: 671, y: 315 },
         { x: 471, y: 315 },
@@ -122,5 +98,216 @@ describe('features/modeling - layout message flows', function() {
       ]);
     })
   );
+
+
+  it('should layout SubProcess -> SubProcess (straight) on SubProcess move',
+    inject(function(elementRegistry, modeling) {
+
+      // given
+      var subProcessShape = elementRegistry.get('SubProcess_G'),
+          messageFlowConnection = elementRegistry.get('MessageFlow_3');
+
+      // when
+      modeling.moveElements([ subProcessShape ], { x: 300, y: 0 });
+
+      // then
+      expect(messageFlowConnection).to.have.waypoints([
+         { x: 902, y: 266 }, { x: 902, y: 458 }
+      ]);
+    })
+  );
+
+
+  describe('should keep task docking', function() {
+
+    describe('on SubProcess resize', function() {
+
+      it('SubProcess -> Task (straight)',
+        inject(function(elementRegistry, modeling) {
+
+          // given
+          var subProcessShape = elementRegistry.get('SubProcess_G'),
+              messageFlowConnection = elementRegistry.get('MessageFlow_7');
+
+          // when
+          modeling.resizeShape(subProcessShape, {
+            x: 586,
+            y: 458,
+            width: 212,
+            height: 122
+          });
+
+          // then
+          // expect cropped, repaired manhattan connection
+          expect(messageFlowConnection).to.have.waypoints([
+            { x: 752, y: 458 },
+            { x: 752, y: 214 }
+          ]);
+        })
+      );
+    });
+
+
+    describe('on SubProcess move', function() {
+
+      it('SubProcess -> Task (straight)',
+        inject(function(elementRegistry, modeling) {
+
+          // given
+          var subProcessShape = elementRegistry.get('SubProcess_G'),
+              messageFlowConnection = elementRegistry.get('MessageFlow_7');
+
+          // when
+          modeling.moveElements([ subProcessShape ], { x: 50, y: 0 });
+
+          // then
+          // expect cropped, repaired manhattan connection
+          expect(messageFlowConnection).to.have.waypoints([
+            { x: 752, y: 458 },
+            { x: 752, y: 214 }
+          ]);
+        })
+      );
+    });
+
+
+    describe('on Participant move', function() {
+
+      it('Task -> Participant (straight)',
+        inject(function(elementRegistry, modeling) {
+
+          // given
+          var participantShape = elementRegistry.get('Participant_B'),
+              messageFlowConnection_1 = elementRegistry.get('MessageFlow_1'),
+              messageFlowConnection_6 = elementRegistry.get('MessageFlow_6');
+
+          // when
+          modeling.moveElements([ participantShape ], { x: 300, y: 50 });
+          // then
+
+          // expect cropped, repaired manhattan connection
+          expect(messageFlowConnection_1).to.have.waypoints([
+            { original: { x: 590, y: 214 }, x: 590, y: 214 },
+            { original: { x: 590, y: 465 }, x: 590, y: 465 }
+          ]);
+
+          expect(messageFlowConnection_6).to.have.waypoints([
+            { original: { x: 773, y: 465 }, x: 773, y: 465 },
+            { original: { x: 773, y: 214 }, x: 773, y: 214 }
+          ]);
+        })
+      );
+
+    });
+
+
+    describe('on Participant resize', function() {
+
+      it('Task -> Participant (straight)',
+        inject(function(elementRegistry, modeling) {
+
+          // given
+          var participantShape = elementRegistry.get('Participant_B'),
+              messageFlowConnection = elementRegistry.get('MessageFlow_1');
+
+          // when
+          modeling.resizeShape(participantShape, {
+            x: 222,
+            y: 415,
+            width: 580,
+            height: 185
+          });
+
+          // then
+          // expect cropped, repaired manhattan connection
+          expect(messageFlowConnection).to.have.waypoints([
+            { x: 590, y: 214 },
+            { x: 590, y: 415 }
+          ]);
+        })
+      );
+
+
+      it('Participant -> Task (straight)',
+        inject(function(elementRegistry, modeling) {
+
+          // given
+          var participantShape = elementRegistry.get('Participant_B'),
+              messageFlowConnection = elementRegistry.get('MessageFlow_6');
+
+          // when
+          modeling.resizeShape(participantShape, {
+            x: 222,
+            y: 415,
+            width: 580,
+            height: 185
+          });
+
+          // then
+          // expect cropped, repaired manhattan connection
+          expect(messageFlowConnection).to.have.waypoints([
+            { x: 773, y: 415 },
+            { x: 773, y: 214 }
+          ]);
+        })
+      );
+
+
+      it('Task -> Participant (manhattan)',
+        inject(function(elementRegistry, modeling) {
+
+          // given
+          var participantShape = elementRegistry.get('Participant_B'),
+              messageFlowConnection = elementRegistry.get('MessageFlow_1');
+
+          // when
+          modeling.resizeShape(participantShape, {
+            x: 622,
+            y: 415,
+            width: 600,
+            height: 185
+          });
+
+          // then
+          // expect cropped, repaired manhattan connection
+          expect(messageFlowConnection).to.have.waypoints([
+            { x: 590, y: 214 },
+            { x: 590, y: 315 },
+            { x: 990, y: 315 },
+            { x: 990, y: 415 }
+          ]);
+        })
+      );
+
+
+      it('Participant -> Task (manhattan)',
+        inject(function(elementRegistry, modeling) {
+
+          // given
+          var participantShape = elementRegistry.get('Participant_B'),
+              messageFlowConnection = elementRegistry.get('MessageFlow_6');
+
+          // when
+          modeling.resizeShape(participantShape, {
+            x: 222,
+            y: 415,
+            width: 500,
+            height: 185
+          });
+
+          // then
+          // expect cropped, repaired manhattan connection
+          expect(messageFlowConnection).to.have.waypoints([
+             { x: 681, y: 415 },
+             { x: 681, y: 315 },
+             { x: 773, y: 315 },
+             { x: 773, y: 214 }
+          ]);
+        })
+      );
+
+    });
+
+  });
 
 });
