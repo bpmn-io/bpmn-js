1f667e0184c49bb09f1ef21c4dc3885f1b09b58b
Philipp Fromme <philipp.fromme@camunda.com>
2017-01-09 14:30:39 +0100

    fix(move-to-origin): ignore children of subprocesses
    Closes camunda/camunda-modeler#444

--- test/spec/features/editor-actions/BpmnEditorActionsSpec.js
+++ test/spec/features/editor-actions/BpmnEditorActionsSpec.js
@@ -8,12 +8,14 @@ var pick = require('lodash/object/pick');
 
 var getBBox = require('diagram-js/lib/util/Elements').getBBox;
 
+var getParent = require('lib/features/modeling/util/ModelingUtil').getParent;
+
 
 var bpmnEditorActionsModule = require('../../../../lib/features/editor-actions'),
     modelingModule = require('../../../../lib/features/modeling'),
     coreModule = require('../../../../lib/core');
 
-var basicXML = require('../../../fixtures/bpmn/simple.bpmn'),
+var basicXML = require('../../../fixtures/bpmn/nested-subprocesses.bpmn'),
     collaborationXML = require('../../../fixtures/bpmn/collaboration.bpmn');
 
 
@@ -47,6 +49,23 @@ describe('features/editor-actions', function() {
     describe('single process', testMoveToOrigin(basicXML));
 
     describe('collaboration', testMoveToOrigin(collaborationXML));
+
+    describe('subprocesses', function() {
+
+      beforeEach(bootstrapModeler(basicXML, { modules: [ bpmnEditorActionsModule, modelingModule, coreModule ] }));
+
+      it('should ignore children of subprocesses', inject(function(editorActions, elementRegistry) {
+        // given
+        var startEvent = elementRegistry.get('StartEvent_3'),
+            startEventParent = getParent(startEvent);
+
+        // when
+        editorActions.trigger('moveToOrigin');
+
+        // then
+        expect(getParent(startEvent)).to.equal(startEventParent);
+      }));
+    });
 
   });
 
