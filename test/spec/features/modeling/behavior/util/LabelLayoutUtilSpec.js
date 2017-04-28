83f55b1fb1ccfe7558e2ac7c9b116a8874c8fb1c
Nico Rehwaldt <git_nikku@nixis.de>
2017-04-28 16:25:53 +0200

    fix(modeling): handle label layouting edge case
    Related to #669

---
+++ test/spec/features/modeling/behavior/util/LabelLayoutUtilSpec.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var getLabelAdjustment = require('lib/features/modeling/behavior/util/LabelLayoutUtil').getLabelAdjustment;
+
+
+describe('modeling/behavior/util - LabelLayoutUtil#getLabelAdjustment', function() {
+
+  describe('should recognize on the line label', function() {
+
+    var newLine = [
+      { x: 10, y: 10 },
+      // -
+      { x: 15, y: 10 },
+      // |
+      { x: 15, y: 5 },
+      // -
+      { x: 30, y: 5 }
+    ];
+
+
+    it('horizontal', function() {
+
+      // given
+      var line = [
+        { x: 10, y: 10 },
+        // -
+        { x: 20, y: 10 }
+      ];
+
+      // label with center { x: 5, y: 10 }
+
+      var label = {
+        x: 0,
+        y: 5,
+        width: 10,
+        height: 10
+      };
+
+      // when
+      var delta = getLabelAdjustment(label, newLine, line, { connectionStart: true });
+
+      // then
+      expect(delta).to.eql({ x: 0, y: 0 });
+    });
+
+
+    it('zero-length line', function() {
+
+      // given
+      var line = [
+        { x: 10, y: 10 },
+        // -
+        { x: 10, y: 10 }
+      ];
+
+      // label with center { x: 5, y: 10 }
+
+      var label = {
+        x: 0,
+        y: 5,
+        width: 10,
+        height: 10
+      };
+
+      // when
+      var delta = getLabelAdjustment(label, newLine, line, { connectionStart: true });
+
+      // then
+      expect(delta).to.eql({ x: 0, y: 0 });
+    });
+
+  });
+
+
+});
