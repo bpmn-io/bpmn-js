e4a2c6f534bcd92a963fe999829257197da03db2
felixlinker <linkerfelix@gmail.com>
2017-12-01 14:45:54 +0100

    chore(renderer): Created new module BpmnRenderUtil

---
+++ test/spec/util/BpmnRenderUtilSpec.js
@@ -0,0 +1,77 @@
+'use strict';
+
+require('../../TestHelper');
+
+var BpmnRenderUtil = require('../../../lib/util/BpmnRenderUtil');
+
+describe('BpmnRenderUtil', function() {
+
+  it('should expose isTypedEvent', function() {
+
+    expect(BpmnRenderUtil.isTypedEvent).to.be.a('function');
+
+  });
+
+  it('should expose isThrowEvent', function() {
+
+    expect(BpmnRenderUtil.isThrowEvent).to.be.a('function');
+
+  });
+
+  it('should expose isCollection', function() {
+
+    expect(BpmnRenderUtil.isCollection).to.be.a('function');
+
+  });
+
+
+  it('should expose getDi', function() {
+
+    expect(BpmnRenderUtil.getDi).to.be.a('function');
+
+  });
+
+
+  it('should expose getSemantic', function() {
+
+    expect(BpmnRenderUtil.getSemantic).to.be.a('function');
+
+  });
+
+  it('should expose getCirclePath', function() {
+
+    expect(BpmnRenderUtil.getCirclePath).to.be.a('function');
+
+  });
+
+  it('should expose getRoundRectPath', function() {
+
+    expect(BpmnRenderUtil.getRoundRectPath).to.be.a('function');
+
+  });
+
+  it('should expose getDiamondPath', function() {
+
+    expect(BpmnRenderUtil.getDiamondPath).to.be.a('function');
+
+  });
+
+  it('should expose getRectPath', function() {
+
+    expect(BpmnRenderUtil.getRectPath).to.be.a('function');
+
+  });
+
+  it('should expose getFillColor', function() {
+
+    expect(BpmnRenderUtil.getFillColor).to.be.a('function');
+
+  });
+
+  it('should expose getStrokeColor', function() {
+
+    expect(BpmnRenderUtil.getStrokeColor).to.be.a('function');
+
+  });
+
+});
