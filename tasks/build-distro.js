d9d0d9dcdb768c1c89e749f17d3c19bdfd41c398
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-01-08 09:19:19 +0100

    chore(distro): consolidate into single task

--- tasks/build-distro.js
+++ tasks/build-distro.js
@@ -2,7 +2,34 @@
 
 var bundle = require('./bundle');
 
-bundle({
+var path = require('path');
+
+var mkdirp = require('mkdirp').sync,
+    cp = require('cpx').copySync,
+    del = require('del').sync;
+
+var dest = 'dist';
+
+
+function resolve(module, sub) {
+  var pkg = require.resolve(module + '/package.json');
+
+  return path.dirname(pkg) + sub;
+}
+
+console.log('clean ' + dest);
+del(dest);
+
+console.log('mkdir -p ' + dest);
+mkdirp(dest);
+
+console.log(`copy bpmn-font to ${dest}/bpmn-font`);
+cp('assets/bpmn-font/*', dest + '/assets/bpmn-font');
+
+console.log(`copy diagram-js.css to ${dest}`);
+cp(resolve('diagram-js', '/assets/**'), dest + '/assets');
+
+bundle(dest, {
   'bpmn-viewer': 'lib/Viewer.js',
   'bpmn-navigated-viewer': 'lib/NavigatedViewer.js',
   'bpmn-modeler': 'lib/Modeler.js'
