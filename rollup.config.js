3c895e08ae330530691beec4cb0794b9c37ffe14
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-04-02 23:15:15 +0200

    chore(project): build pre-packaged distros with Rollup
    * enables tree-shaking
    * simplifies build

---
+++ rollup.config.js
@@ -0,0 +1,140 @@
+import uglify from 'rollup-plugin-uglify';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import license from 'rollup-plugin-license';
+import replace from 'rollup-plugin-replace';
+
+import {
+  readFileSync
+} from 'fs';
+
+import pkg from './package.json';
+
+const outputDir = 'dist';
+
+const distros = [
+  {
+    input: 'Viewer',
+    output: 'bpmn-viewer'
+  },
+  {
+    input: 'NavigatedViewer',
+    output: 'bpmn-navigated-viewer'
+  },
+  {
+    input: 'Modeler',
+    output: 'bpmn-modeler'
+  }
+];
+
+const configs = distros.reduce(function(configs, distro) {
+  const {
+    input,
+    output
+  } = distro;
+
+  return [
+    ...configs,
+    {
+      input: `./lib/${input}.js`,
+      output: {
+        name: 'BpmnJS',
+        file: `${outputDir}/${output}.development.js`,
+        format: 'umd'
+      },
+      plugins: pgl([
+        banner(output)
+      ])
+    },
+    {
+      input: `./lib/${input}.js`,
+      output: {
+        name: 'BpmnJS',
+        file: `${outputDir}/${output}.production.min.js`,
+        format: 'umd'
+      },
+      plugins: pgl([
+        banner(output, true),
+        uglify({
+          output: {
+            comments: /license|@preserve/
+          }
+        })
+      ])
+    }
+  ];
+}, []);
+
+export default configs;
+
+
+// helpers //////////////////////
+
+function banner(bundleName, minified) {
+
+  const bannerName = (
+    minified
+      ? 'banner-min'
+      : 'banner'
+  );
+
+  const bannerTemplate = readFileSync(`${__dirname}/resources/${bannerName}.txt`, 'utf8');
+
+  const banner = processTemplate(bannerTemplate, {
+    version: pkg.version,
+    date: today(),
+    name: bundleName
+  });
+
+  return license({
+    banner
+  });
+}
+
+function pgl(plugins=[]) {
+  return [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    nodeResolve({
+      module: true,
+      main: true,
+      browser: true
+    }),
+    commonjs(),
+    json(),
+    ...plugins
+  ];
+}
+
+function pad(n) {
+  if (n < 10) {
+    return '0' + n;
+  } else {
+    return n;
+  }
+}
+
+function today() {
+  const d = new Date();
+
+  return [
+    d.getFullYear(),
+    pad(d.getMonth() + 1),
+    pad(d.getDay())
+  ].join('-');
+}
+
+function processTemplate(str, args) {
+  return str.replace(/\{\{\s*([^\s]+)\s*\}\}/g, function(_, n) {
+
+    var replacement = args[n];
+
+    if (!replacement) {
+      throw new Error('unknown template {{ ' + n + '}}');
+    }
+
+    return replacement;
+  });
+}
--- tasks/build-distro.js
+++ tasks/build-distro.js
@@ -1,10 +1,9 @@
 'use strict';
 
-var bundle = require('./bundle');
-
 var path = require('path');
 
-var mkdirp = require('mkdirp').sync,
+var exec = require('execa').sync,
+    mkdirp = require('mkdirp').sync,
     cp = require('cpx').copySync,
     del = require('del').sync;
 
@@ -29,13 +28,23 @@ cp(resolve('bpmn-font', '/dist/{font,css}/**'), dest + '/assets/bpmn-font');
 console.log('copy diagram-js.css to ' + dest);
 cp(resolve('diagram-js', '/assets/**'), dest + '/assets');
 
-bundle(dest, {
-  'bpmn-viewer': 'lib/Viewer.js',
-  'bpmn-navigated-viewer': 'lib/NavigatedViewer.js',
-  'bpmn-modeler': 'lib/Modeler.js'
-}, function(err) {
+console.log('building pre-packaged distributions');
 
-  if (err) {
-    console.error('bundling failed', err);
+var NODE_ENV = process.env.NODE_ENV;
+
+[ 'production', 'development' ].forEach(function(env) {
+
+  try {
+    process.env.NODE_ENV = env;
+
+    exec('rollup', [ '-c' ]);
+  } catch (e) {
+    console.error('failed to build pre-package distributions', e);
+
+    process.exit(1);
   }
+
+  process.env.NODE_ENV = NODE_ENV;
 });
+
+console.log('done.');
