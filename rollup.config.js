61ffd5832892df4afa8d2c69571a2b19e4c39e93
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-10-25 20:52:50 +0200

    chore(project): bump devDependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
