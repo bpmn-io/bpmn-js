051947927fcab4e311d9c972c20051e2c6e2024b
Daniel Newport <dan@newport.solutions>
2019-03-18 07:29:37 -0400

    skip running module distro tests in unsupported browsers

--- tasks/test-distro.js
+++ tasks/test-distro.js
@@ -3,6 +3,19 @@ var execSync = require('execa').sync;
 var failures = 0;
 
 function runTest(variant, env, isModule = false) {
+  // when running distro tests for JavaScript modules PhantomJS is not supported.
+  // If there is not at least one browser left then skip the test.
+  var browsers = (process.env.TEST_BROWSERS || 'PhantomJS')
+    .replace(/^\s+|\s+$/, '')
+    .split(/\s*,\s*/g)
+    .filter(browser => !isModule || (isModule && browser != 'PhantomJS'));
+
+  if (browsers.length === 0) {
+    console.log('[SKIPPED] ' + variant + '@' + env + ' :: No supported browser specified');
+    return;
+  }
+
+  process.env.TEST_BROWSERS = browsers.join(',');
 
   var NODE_ENV = process.env.NODE_ENV;
 
