8eced8a3fb232dfacff2e4f1e9322b53d1572323
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2019-04-09 14:35:48 +0200

    test(modeling): ensure layout is done after element removal
    * adjust test suite to pass with diagram-js@3.2
    * verify new cropping behavior
    * verify #940 is fixed

--- test/spec/features/modeling/DeleteShapeSpec.js
+++ test/spec/features/modeling/DeleteShapeSpec.js
@@ -7,14 +7,14 @@ import modelingModule from 'lib/features/modeling';
 import coreModule from 'lib/core';
 
 
+var testModules = [ coreModule, modelingModule ];
+
+
 describe('features/modeling - #removeShape', function() {
 
   var diagramXML = require('../../../fixtures/bpmn/sequence-flows.bpmn');
 
-  var testModules = [ coreModule, modelingModule ];
-
   beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
-
 
   describe('shape handling', function() {
 
@@ -69,5 +69,36 @@ describe('features/modeling - #removeShape', function() {
       expect(task.$parent).to.be.null;
     }));
   });
+
+});
+
+
+describe('features/modeling - #removeShape', function() {
+
+  var diagramXML = require('./DeleteShape.cropping.bpmn');
+
+  beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
+
+
+  it('should crop waypoints on undo/redo', inject(
+    function(elementRegistry, commandStack, modeling) {
+
+      // given
+      var taskShape = elementRegistry.get('Task_A'),
+          sequenceFlowConnection = elementRegistry.get('SequenceFlow_1'),
+          incomingFlow = taskShape.incoming[0],
+          outgoingFlow = taskShape.outgoing[0],
+          expectedStart = incomingFlow.waypoints[0],
+          expectedEnd = outgoingFlow.waypoints[1];
+
+      // when
+      modeling.removeShape(taskShape);
+      commandStack.undo();
+      commandStack.redo();
+
+      // then
+      expect(sequenceFlowConnection).to.have.waypoints([ expectedStart, expectedEnd ]);
+    }
+  ));
 
 });
