b86b26accbbdd2607b2fa4e861fb586670bddf92
Philipp Fromme <philippfromme@outlook.com>
2019-08-06 18:57:30 +0200

    feat(create): create multiple elements
    * add <elements.create> rule for creating multiple elements
    * handle creating multiple elements in CreateParticipantBehavior
    * create sub process with start event through palette

--- test/spec/features/palette/PaletteProviderSpec.js
+++ test/spec/features/palette/PaletteProviderSpec.js
@@ -1,11 +1,17 @@
 import {
   bootstrapModeler,
+  getBpmnJS,
   inject
 } from 'test/TestHelper';
 
+import coreModule from 'lib/core';
+import createModule from 'diagram-js/lib/features/create';
 import modelingModule from 'lib/features/modeling';
 import paletteModule from 'lib/features/palette';
-import coreModule from 'lib/core';
+
+import { createMoveEvent } from 'diagram-js/lib/features/mouse/Mouse';
+
+import { is } from 'lib/util/ModelUtil';
 
 import {
   query as domQuery,
@@ -17,12 +23,17 @@ describe('features/palette', function() {
 
   var diagramXML = require('../../../fixtures/bpmn/features/replace/01_replace.bpmn');
 
-  var testModules = [ coreModule, modelingModule, paletteModule ];
+  var testModules = [
+    coreModule,
+    createModule,
+    modelingModule,
+    paletteModule
+  ];
 
   beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
 
 
-  it('should provide BPMN modeling palette', inject(function(canvas, palette) {
+  it('should provide BPMN modeling palette', inject(function(canvas) {
 
     // when
     var paletteElement = domQuery('.djs-palette', canvas._container);
@@ -32,4 +43,35 @@ describe('features/palette', function() {
     expect(entries.length).to.equal(14);
   }));
 
+
+  describe('sub process', function() {
+
+    it('should create sub process with start event', inject(function(dragging, palette) {
+
+      // when
+      triggerPaletteEntry('create.subprocess-expanded');
+
+      // then
+      var context = dragging.context(),
+          elements = context.data.elements;
+
+      expect(elements).to.have.length(2);
+      expect(is(elements[0], 'bpmn:SubProcess')).to.be.true;
+      expect(is(elements[1], 'bpmn:StartEvent')).to.be.true;
+    }));
+
+  });
+
 });
+
+// helpers //////////
+
+function triggerPaletteEntry(id) {
+  getBpmnJS().invoke(function(palette) {
+    var entry = palette.getEntries()[ id ];
+
+    if (entry && entry.action && entry.action.click) {
+      entry.action.click(createMoveEvent(0, 0));
+    }
+  });
+}
