25d2a7878b8127c970c66e7103a039da2c9bcfe0
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2021-01-06 12:04:20 +0100

    chore: make ChromeHeadless the default test browser during development

--- test/config/karma.distro.js
+++ test/config/karma.distro.js
@@ -1,6 +1,6 @@
 // configures browsers to run test against
 // any of [ 'ChromeHeadless', 'Chrome', 'Firefox', 'IE', 'PhantomJS' ]
-var browsers = (process.env.TEST_BROWSERS || 'PhantomJS').split(',');
+var browsers = (process.env.TEST_BROWSERS || 'ChromeHeadless').split(',');
 
 // use puppeteer provided Chrome for testing
 process.env.CHROME_BIN = require('puppeteer').executablePath();
