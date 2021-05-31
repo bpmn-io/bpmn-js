d7023ac9524e0aa55875567282c4f7068e6023dd
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-05-31 18:07:01 +0200

    detect mixed diagram

--- test/spec/features/mixed-diagram-support/MixedDiagramSupportSpec.js
+++ test/spec/features/mixed-diagram-support/MixedDiagramSupportSpec.js
@@ -5,26 +5,77 @@ import {
 
 import modelingModule from 'lib/features/modeling';
 import coreModule from 'lib/core';
+import mixedDiagramSupportModule from 'lib/features/mixed-diagram-support';
 
 
 describe('features/mixed-diagram-support', function() {
 
   var diagramXML = require('../../../fixtures/bpmn/import/mixed.bpmn');
 
-  var testModules = [ coreModule, modelingModule ];
+  var testModules = [
+    coreModule,
+    modelingModule,
+    mixedDiagramSupportModule
+  ];
   beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
 
 
-  describe('appending shape', function() {
+  describe.only('init', function() {
+
+    function verify(diagram, expected) {
+      return function() {
+        var bootstrap = bootstrapModeler(diagram, { modules: testModules });
+
+        // when
+        return bootstrap()
+          .then(inject(function(mixedDiagramSupport) {
+
+            // then
+            expect(mixedDiagramSupport).to.have.property('initialized', expected);
+          }));
+      };
+    }
+
+
+    it('should initialize on a mixed diagram', verify(
+      require('../../../fixtures/bpmn/import/mixed.bpmn'),
+      true
+    ));
+
+
+    it('should NOT initialize on a collaboration diagram', verify(
+      require('../../../fixtures/bpmn/import/collaboration.bpmn'),
+      false
+    ));
+
+
+    it('should NOT initialize on a process diagram', verify(
+      require('../../../fixtures/bpmn/import/process.bpmn'),
+      false
+    ));
+  });
+
+
+  describe.skip('appending shape', function() {
+
 
     it.only('should execute', inject(function(elementRegistry, modeling) {
 
       // given
       var task = elementRegistry.get('Task_2');
+      console.log(task.parent, task.businessObject.$parent);
 
       // when
-      var targetShape = modeling.appendShape(task, { type: 'bpmn:Task' }),
-          target = targetShape.businessObject;
+      try {
+
+        var targetShape = modeling.appendShape(task, { type: 'bpmn:Task' }),
+            target = targetShape.businessObject;
+      } finally {
+
+        console.log(task.parent, task.businessObject.$parent);
+      }
+
+
 
       // then
       expect(targetShape).to.exist;
