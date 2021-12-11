f3d06f361b1064462a6a9df64d62849547e4ea11
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2021-12-11 15:00:30 +0100

    feat(util/ModelUtil): expose `isAny`
    It is a core utility that will be tree-shaken if not used.
    
    We'll continue to expose it via `ModelingUtil` for backwards
    compatibility.

--- test/spec/util/ModelUtilSpec.js
+++ test/spec/util/ModelUtilSpec.js
@@ -8,6 +8,7 @@ import modelingModule from 'lib/features/modeling';
 
 import {
   is,
+  isAny,
   getDi
 } from 'lib/util/ModelUtil';
 
@@ -24,51 +25,85 @@ describe('util/ModelUtil', function() {
   }));
 
 
-  it('should work with diagram element', inject(function(elementFactory) {
+  describe('#is', function() {
 
-    // given
-    var messageFlowConnection = elementFactory.createConnection({ type: 'bpmn:MessageFlow' });
+    it('should work with diagram element', inject(function(elementFactory) {
 
-    // then
-    expect(is(messageFlowConnection, 'bpmn:MessageFlow')).to.be.true;
-    expect(is(messageFlowConnection, 'bpmn:BaseElement')).to.be.true;
+      // given
+      var messageFlowConnection = elementFactory.createConnection({ type: 'bpmn:MessageFlow' });
 
-    expect(is(messageFlowConnection, 'bpmn:SequenceFlow')).to.be.false;
-    expect(is(messageFlowConnection, 'bpmn:Task')).to.be.false;
-  }));
+      // then
+      expect(is(messageFlowConnection, 'bpmn:MessageFlow')).to.be.true;
+      expect(is(messageFlowConnection, 'bpmn:BaseElement')).to.be.true;
 
-
-  it('should work with business object', inject(function(bpmnFactory) {
-
-    // given
-    var gateway = bpmnFactory.create('bpmn:Gateway');
-
-    // then
-    expect(is(gateway, 'bpmn:Gateway')).to.be.true;
-    expect(is(gateway, 'bpmn:BaseElement')).to.be.true;
-
-    expect(is(gateway, 'bpmn:SequenceFlow')).to.be.false;
-  }));
+      expect(is(messageFlowConnection, 'bpmn:SequenceFlow')).to.be.false;
+      expect(is(messageFlowConnection, 'bpmn:Task')).to.be.false;
+    }));
 
 
-  it('should work with untyped business object', inject(function() {
+    it('should work with business object', inject(function(bpmnFactory) {
 
-    // given
-    var foo = { businessObject: 'BAR' };
+      // given
+      var gateway = bpmnFactory.create('bpmn:Gateway');
 
-    // then
-    expect(is(foo, 'FOO')).to.be.false;
-  }));
+      // then
+      expect(is(gateway, 'bpmn:Gateway')).to.be.true;
+      expect(is(gateway, 'bpmn:BaseElement')).to.be.true;
+
+      expect(is(gateway, 'bpmn:SequenceFlow')).to.be.false;
+    }));
 
 
-  it('should work with untyped diagram element', inject(function() {
+    it('should work with untyped business object', inject(function() {
 
-    // given
-    var foo = { };
+      // given
+      var foo = { businessObject: 'BAR' };
 
-    // then
-    expect(is(foo, 'FOO')).to.be.false;
-  }));
+      // then
+      expect(is(foo, 'FOO')).to.be.false;
+    }));
+
+
+    it('should work with untyped diagram element', inject(function() {
+
+      // given
+      var foo = { };
+
+      // then
+      expect(is(foo, 'FOO')).to.be.false;
+    }));
+
+  });
+
+
+  describe('isAny', function() {
+
+    it('should work on shape', inject(function(bpmnFactory, elementFactory) {
+
+      // given
+      var element = elementFactory.createShape({ type: 'bpmn:Gateway' });
+
+      // then
+      expect(isAny(element, [ 'bpmn:Gateway' ])).to.be.true;
+      expect(isAny(element, [ 'bpmn:SequenceFlow', 'bpmn:Gateway' ])).to.be.true;
+      expect(isAny(element, [ 'bpmn:BaseElement' ])).to.be.true;
+      expect(isAny(element, [ 'bpmn:SequenceFlow' ])).to.be.false;
+    }));
+
+
+    it('should work on businessObject', inject(function(bpmnFactory, elementFactory) {
+
+      // given
+      var businessObject = bpmnFactory.create('bpmn:Gateway');
+
+      // then
+      expect(isAny(businessObject, [ 'bpmn:Gateway' ])).to.be.true;
+      expect(isAny(businessObject, [ 'bpmn:SequenceFlow', 'bpmn:Gateway' ])).to.be.true;
+      expect(isAny(businessObject, [ 'bpmn:BaseElement' ])).to.be.true;
+      expect(isAny(businessObject, [ 'bpmn:SequenceFlow' ])).to.be.false;
+    }));
+
+  });
 
 
   describe('#getDi', function() {
