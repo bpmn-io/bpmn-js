f8d55fe21bdc23eeab99afd6c3283bbc4739846f
Martin Stamm <martin.stamm@camunda.com>
2022-01-14 10:52:24 +0100

    fix(drilldown): set currentRoot on collaboration change

--- test/spec/features/drilldown/DrilldownIntegrationSpec.js
+++ test/spec/features/drilldown/DrilldownIntegrationSpec.js
@@ -20,7 +20,15 @@ describe('features - drilldown', function() {
 
   beforeEach(bootstrapModeler(multiLayerXML, { modules: testModules }));
 
-  describe('Navigation', function() {
+  describe('Navigation - Collaboration', function() {
+
+    var process, participant;
+
+    beforeEach(inject(function(canvas, elementFactory) {
+      process = canvas.getRootElement();
+      participant = elementFactory.createParticipantShape({ x: 100, y: 100 });
+    }));
+
 
     it('should not reset scroll on create collaboration',
       inject(function(canvas, modeling) {
@@ -31,7 +39,8 @@ describe('features - drilldown', function() {
         var zoomedAndScrolledViewbox = canvas.viewbox();
 
         // when
-        modeling.makeCollaboration();
+        modeling.createShape(participant, { x: 0, y: 0 }, process);
+
 
         // then
         expectViewbox(zoomedAndScrolledViewbox);
@@ -48,7 +57,7 @@ describe('features - drilldown', function() {
         var zoomedAndScrolledViewbox = canvas.viewbox();
 
         // when
-        modeling.makeCollaboration();
+        modeling.createShape(participant, { x: 0, y: 0 }, process);
         commandStack.undo();
 
         // then
@@ -66,7 +75,7 @@ describe('features - drilldown', function() {
         var zoomedAndScrolledViewbox = canvas.viewbox();
 
         // when
-        modeling.makeCollaboration();
+        modeling.createShape(participant, { x: 400, y: 225 }, process);
         commandStack.undo();
         commandStack.redo();
 
@@ -74,6 +83,28 @@ describe('features - drilldown', function() {
         expectViewbox(zoomedAndScrolledViewbox);
       })
     );
+
+
+    it('should remember scroll and zoom after morph', inject(function(canvas, modeling) {
+
+      // given
+      canvas.scroll({ dx: 500, dy: 500 });
+      canvas.zoom(0.5);
+      var zoomedAndScrolledViewbox = canvas.viewbox();
+
+      modeling.createShape(participant, { x: 400, y: 225 }, process);
+      var collaboration = canvas.getRootElement();
+
+      // when
+      canvas.setRootElement(canvas.findRoot('collapsedProcess_plane'));
+      canvas.setRootElement(collaboration);
+
+      // then
+      var newViewbox = canvas.viewbox();
+      expect(newViewbox.x).to.eql(zoomedAndScrolledViewbox.x);
+      expect(newViewbox.y).to.eql(zoomedAndScrolledViewbox.y);
+      expect(newViewbox.scale).to.eql(zoomedAndScrolledViewbox.scale);
+    }));
 
   });
 
