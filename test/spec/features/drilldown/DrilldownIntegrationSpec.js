70826c58313205da8921872f1bd7b43bd1360986
Martin Stamm <martin.stamm@camunda.com>
2022-01-21 14:47:14 +0100

    feat(drilldown): update breadcrumbs on name change

--- test/spec/features/drilldown/DrilldownIntegrationSpec.js
+++ test/spec/features/drilldown/DrilldownIntegrationSpec.js
@@ -108,6 +108,51 @@ describe('features - drilldown', function() {
 
   });
 
+
+  describe('Breadcrumbs - Name changes', function() {
+
+    it('should update on plane name change',
+      inject(function(canvas, modeling) {
+
+        // given
+        canvas.setRootElement(canvas.findRoot('collapsedProcess_2_plane'));
+
+        // when
+        modeling.updateProperties(canvas.getRootElement(), { name: 'new name' });
+
+        // then
+        expectBreadcrumbs([
+          'Root',
+          'Collapsed Process',
+          'Expanded Process',
+          'new name'
+        ]);
+      })
+    );
+
+
+    it('should update on element name change',
+      inject(function(canvas, elementRegistry, modeling) {
+
+        // given
+        canvas.setRootElement(canvas.findRoot('collapsedProcess_2_plane'));
+        var shape = elementRegistry.get('collapsedProcess_2');
+
+        // when
+        modeling.updateProperties(shape, { name: 'new name' });
+
+        // then
+        expectBreadcrumbs([
+          'Root',
+          'Collapsed Process',
+          'Expanded Process',
+          'new name'
+        ]);
+      })
+    );
+
+  });
+
 });
 
 
@@ -122,4 +167,20 @@ function expectViewbox(expectedViewbox) {
     expect(viewbox.y).to.eql(expectedViewbox.y);
     expect(viewbox.scale).to.eql(expectedViewbox.scale);
   });
+}
+
+function getBreadcrumbs() {
+  return getBpmnJS().invoke(function(canvas) {
+    return canvas.getContainer().querySelector('.bjs-breadcrumbs');
+  });
+}
+
+function expectBreadcrumbs(expected) {
+  var breadcrumbs = getBreadcrumbs();
+
+  var crumbs = Array.from(breadcrumbs.children).map(function(element) {
+    return element.innerText;
+  });
+
+  expect(crumbs).to.eql(expected);
 }
