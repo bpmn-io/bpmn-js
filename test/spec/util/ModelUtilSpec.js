27ab7c5ec74a8242dfde8ac6245a2be71edfb028
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2022-02-17 11:14:52 +0100

    chore: backport ModelUtil#getDi
    Added with bpmn-js@9 and crucial for plug-ins to operate both on
    bpmn-js@8 and bpmn-js@9.

--- test/spec/util/ModelUtilSpec.js
+++ test/spec/util/ModelUtilSpec.js
@@ -7,7 +7,8 @@ import coreModule from 'lib/core';
 import modelingModule from 'lib/features/modeling';
 
 import {
-  is
+  is,
+  getDi
 } from 'lib/util/ModelUtil';
 
 
@@ -23,50 +24,86 @@ describe('util/ModelUtil', function() {
   }));
 
 
-  it('should work with diagram element', inject(function(elementFactory) {
+  describe('#is', function() {
 
-    // given
-    var messageFlowConnection = elementFactory.createConnection({ type: 'bpmn:MessageFlow' });
+    it('should work with diagram element', inject(function(elementFactory) {
 
-    // then
-    expect(is(messageFlowConnection, 'bpmn:MessageFlow')).to.be.true;
-    expect(is(messageFlowConnection, 'bpmn:BaseElement')).to.be.true;
+      // given
+      var messageFlowConnection = elementFactory.createConnection({ type: 'bpmn:MessageFlow' });
 
-    expect(is(messageFlowConnection, 'bpmn:SequenceFlow')).to.be.false;
-    expect(is(messageFlowConnection, 'bpmn:Task')).to.be.false;
-  }));
+      // then
+      expect(is(messageFlowConnection, 'bpmn:MessageFlow')).to.be.true;
+      expect(is(messageFlowConnection, 'bpmn:BaseElement')).to.be.true;
 
-
-  it('should work with business object', inject(function(bpmnFactory) {
-
-    // given
-    var gateway = bpmnFactory.create('bpmn:Gateway');
-
-    // then
-    expect(is(gateway, 'bpmn:Gateway')).to.be.true;
-    expect(is(gateway, 'bpmn:BaseElement')).to.be.true;
-
-    expect(is(gateway, 'bpmn:SequenceFlow')).to.be.false;
-  }));
+      expect(is(messageFlowConnection, 'bpmn:SequenceFlow')).to.be.false;
+      expect(is(messageFlowConnection, 'bpmn:Task')).to.be.false;
+    }));
 
 
-  it('should work with untyped business object', inject(function() {
+    it('should work with business object', inject(function(bpmnFactory) {
 
-    // given
-    var foo = { businessObject: 'BAR' };
+      // given
+      var gateway = bpmnFactory.create('bpmn:Gateway');
 
-    // then
-    expect(is(foo, 'FOO')).to.be.false;
-  }));
+      // then
+      expect(is(gateway, 'bpmn:Gateway')).to.be.true;
+      expect(is(gateway, 'bpmn:BaseElement')).to.be.true;
+
+      expect(is(gateway, 'bpmn:SequenceFlow')).to.be.false;
+    }));
 
 
-  it('should work with untyped diagram element', inject(function() {
+    it('should work with untyped business object', inject(function() {
 
-    // given
-    var foo = { };
+      // given
+      var foo = { businessObject: 'BAR' };
 
-    // then
-    expect(is(foo, 'FOO')).to.be.false;
-  }));
+      // then
+      expect(is(foo, 'FOO')).to.be.false;
+    }));
+
+
+    it('should work with untyped diagram element', inject(function() {
+
+      // given
+      var foo = { };
+
+      // then
+      expect(is(foo, 'FOO')).to.be.false;
+    }));
+
+  });
+
+
+  describe('#getDi', function() {
+
+    it('should work with connection', inject(function(elementFactory) {
+
+      // given
+      var connection = elementFactory.createConnection({ type: 'bpmn:MessageFlow' });
+
+      // when
+      var di = getDi(connection);
+
+      // then
+      expect(di).to.exist;
+      expect(is(di, 'bpmndi:BPMNEdge')).to.be.true;
+    }));
+
+
+    it('should work with shape', inject(function(elementFactory) {
+
+      // given
+      var shape = elementFactory.createShape({ type: 'bpmn:ServiceTask' });
+
+      // when
+      var di = getDi(shape);
+
+      // then
+      expect(di).to.exist;
+      expect(is(di, 'bpmndi:BPMNShape')).to.be.true;
+    }));
+
+  });
 
 });
