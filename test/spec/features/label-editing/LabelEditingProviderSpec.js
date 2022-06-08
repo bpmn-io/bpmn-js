59c7696ce03222f20eb7ed43f962582459f9fcde
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2022-06-08 13:51:34 +0200

    fix: cancel direct editing before element deletion
    This is take two on fixing #1664.
    
    Without this fix selection change would trigger direct editing
    completion before the delete listerer could cancel it.
    
    Closes #1664

--- test/spec/features/label-editing/LabelEditingProviderSpec.js
+++ test/spec/features/label-editing/LabelEditingProviderSpec.js
@@ -205,12 +205,33 @@ describe('features - label-editing', function() {
 
 
     it('should cancel on element deletion', inject(
+      function(elementRegistry, directEditing, modeling) {
+
+        // given
+        var shape = elementRegistry.get('Task_1'),
+            task = shape.businessObject;
+
+        directEditing.activate(shape);
+
+        directEditing._textbox.content.textContent = 'FOO BAR';
+
+        // when
+        modeling.removeElements([ shape ]);
+
+        // then
+        expect(task.name).not.to.equal('FOO BAR');
+      }
+    ));
+
+
+    it('should cancel on selected element deletion', inject(
       function(elementRegistry, directEditing, selection, modeling) {
 
         // given
         var shape = elementRegistry.get('Task_1'),
             task = shape.businessObject;
 
+        selection.select(shape);
         directEditing.activate(shape);
 
         directEditing._textbox.content.textContent = 'FOO BAR';
