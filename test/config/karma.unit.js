1c4d89075c50c2929b0a5369547243b69f78bd26
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2022-08-19 10:39:09 +0200

    deps: update to webpack@5
    + drop raw-loader (replaced by webpack built-ins)

--- test/config/karma.unit.js
+++ test/config/karma.unit.js
@@ -66,7 +66,7 @@ module.exports = function(karma) {
           },
           {
             test: /\.css|\.bpmn$/,
-            use: 'raw-loader'
+            type: 'asset/source'
           }
         ].concat(coverage ?
           {
