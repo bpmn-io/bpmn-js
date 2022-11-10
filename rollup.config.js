368700cedff4106bb9146b943552545d996c70c6
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2022-11-10 23:27:49 +0100

    chore: move to @rollup* bundling plug-ins

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 /* eslint-env node */
 
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
 import license from 'rollup-plugin-license';
-import replace from 'rollup-plugin-replace';
 
 import {
   readFileSync
@@ -97,6 +97,7 @@ function banner(bundleName, minified) {
 function pgl(plugins = [], env = 'production') {
   return [
     replace({
+      preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify(env)
     }),
     nodeResolve(),
