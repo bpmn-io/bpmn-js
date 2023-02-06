3f1cfec47be8e3b806d2e3b53c6d64ac4b5c48e5
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-02-06 14:05:09 +0100

    feat: allow `BaseViewer#getModule` override with context

--- test/spec/BaseViewerSpec.js
+++ test/spec/BaseViewerSpec.js
@@ -1,5 +1,10 @@
 import BaseViewer from 'lib/BaseViewer';
 
+import inherits from 'inherits-browser';
+
+
+const spy = sinon.spy;
+
 
 describe('BaseViewer', function() {
 
@@ -13,6 +18,47 @@ describe('BaseViewer', function() {
     expect(instance.saveXML).to.exist;
 
     expect(instance instanceof BaseViewer).to.be.true;
+  });
+
+
+  describe('#getModule', function() {
+
+    it('should allow override with context', function() {
+
+      // given
+      const options = {
+        __foo: 1,
+        some: {
+          other: {
+            thing: 'yes'
+          }
+        }
+      };
+
+      function SpecialViewer(options) {
+        this.getModules = spy(function(localOptions) {
+          expect(localOptions, 'options are passed').to.exist;
+
+          expect(localOptions).to.include(options);
+
+          return BaseViewer.prototype.getModules.call(this, localOptions);
+        });
+
+        BaseViewer.call(this, options);
+      }
+
+      inherits(SpecialViewer, BaseViewer);
+
+      // when
+      var instance = new SpecialViewer(options);
+
+      // then
+      expect(instance.getModules).to.have.been.calledOnce;
+
+      expect(instance instanceof SpecialViewer).to.be.true;
+      expect(instance instanceof BaseViewer).to.be.true;
+    });
+
   });
 
 });
