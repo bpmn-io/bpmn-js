23359c9858ecdc705689d55efd50c9913eb86982
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-03-09 14:03:26 +0100

    feat(model): add shape / connection like

--- lib/model/index.d.ts
+++ lib/model/index.d.ts
@@ -1,5 +1,8 @@
-import { ElementLike } from "diagram-js/lib/core";
+import { ElementLike, ShapeLike, ConnectionLike } from "diagram-js/lib/core";
 
 export type ModdleElement = Object;
 
 export type DiagramElement = ElementLike;
+
+export type ShapeLike = ShapeLike;
+export type ConnectionLike = ConnectionLike;
