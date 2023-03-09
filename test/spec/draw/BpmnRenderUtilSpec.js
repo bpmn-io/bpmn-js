31b5af5aa1db66c849b76b2c5042be78c2baaeb0
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-03-09 13:54:11 +0100

    style: improve test formatting

--- test/spec/draw/BpmnRenderUtilSpec.js
+++ test/spec/draw/BpmnRenderUtilSpec.js
@@ -17,77 +17,62 @@ import {
 describe('BpmnRenderUtil', function() {
 
   it('should expose isTypedEvent', function() {
-
     expect(isTypedEvent).to.be.a('function');
-
   });
+
 
   it('should expose isThrowEvent', function() {
-
     expect(isThrowEvent).to.be.a('function');
-
   });
 
+
   it('should expose isCollection', function() {
-
     expect(isCollection).to.be.a('function');
-
   });
 
 
   it('should expose getDi', function() {
-
     expect(getDi).to.be.a('function');
-
   });
 
 
   it('should expose getSemantic', function() {
-
     expect(getSemantic).to.be.a('function');
-
   });
+
 
   it('should expose getCirclePath', function() {
-
     expect(getCirclePath).to.be.a('function');
-
   });
+
 
   it('should expose getRoundRectPath', function() {
-
     expect(getRoundRectPath).to.be.a('function');
-
   });
+
 
   it('should expose getDiamondPath', function() {
-
     expect(getDiamondPath).to.be.a('function');
-
   });
+
 
   it('should expose getRectPath', function() {
-
     expect(getRectPath).to.be.a('function');
-
   });
+
 
   it('should expose getFillColor', function() {
-
     expect(getFillColor).to.be.a('function');
-
   });
 
+
   it('should expose getStrokeColor', function() {
-
     expect(getStrokeColor).to.be.a('function');
-
   });
 
 
   it('should expose getLabelColor', function() {
-
     expect(getLabelColor).to.be.a('function');
-
   });
+
 });
