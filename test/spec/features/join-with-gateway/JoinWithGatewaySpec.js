a0e0141260cf9b424d97d173e0e3052b227ecbbb
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2023-12-15 15:35:39 +0100

    feat: implement command

---
+++ test/spec/features/join-with-gateway/JoinWithGatewaySpec.js
@@ -0,0 +1,56 @@
+import {
+  bootstrapModeler,
+  inject
+} from 'test/TestHelper';
+
+import {
+  is
+} from 'lib/util/ModelUtil';
+
+import modelingModule from 'lib/features/modeling';
+import coreModule from 'lib/core';
+import joinWithGatewayModule from 'lib/features/join-with-gateway';
+
+import diagramXML from './JoinWithGateway.bpmn';
+
+
+const testModules = [ coreModule, modelingModule, joinWithGatewayModule ];
+
+describe('features/join-with-gateway', function() {
+
+
+  beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
+
+
+  it('should execute', inject(function(joinWithGateway) {
+
+    // given
+    const shapes = [
+      element('Task_1'),
+      element('Task_2'),
+      element('Task_3')
+    ];
+
+    // when
+    joinWithGateway.join(shapes);
+
+    // then
+    const gateway = element('Task_1').outgoing[0].target;
+
+    expect(gateway).to.exist;
+    expect(is(gateway, 'bpmn:ExclusiveGateway')).to.be.true;
+
+    shapes.forEach(shape => {
+      expect(shape.outgoing[0].target).to.eql(gateway);
+    });
+
+    expect(gateway.x).to.eql(525);
+    expect(gateway.y).to.eql(92);
+  }));
+});
+
+function element(id) {
+  return inject(function(elementRegistry) {
+    return elementRegistry.get(id);
+  })();
+}
