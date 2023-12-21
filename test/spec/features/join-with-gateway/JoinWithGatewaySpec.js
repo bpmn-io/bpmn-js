0c0170195c50b32e874ef8b5f27515d483a79819
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2023-12-21 16:36:22 +0100

    feat: make context pad optional

--- test/spec/features/join-with-gateway/JoinWithGatewaySpec.js
+++ test/spec/features/join-with-gateway/JoinWithGatewaySpec.js
@@ -47,6 +47,55 @@ describe('features/join-with-gateway', function() {
     expect(gateway.x).to.eql(525);
     expect(gateway.y).to.eql(92);
   }));
+
+
+  it('should undo', inject(function(commandStack, joinWithGateway) {
+
+    // given
+    const shapes = [
+      element('Task_1'),
+      element('Task_2'),
+      element('Task_3')
+    ];
+
+    // when
+    joinWithGateway.join(shapes);
+    commandStack.undo();
+
+    // then
+    shapes.forEach(shape => {
+      expect(shape.outgoing).to.be.empty;
+    });
+  }));
+
+
+  it('should redo', inject(function(commandStack, joinWithGateway) {
+
+    // given
+    const shapes = [
+      element('Task_1'),
+      element('Task_2'),
+      element('Task_3')
+    ];
+
+    // when
+    joinWithGateway.join(shapes);
+    commandStack.undo();
+    commandStack.redo();
+
+    // then
+    const gateway = element('Task_1').outgoing[0].target;
+
+    expect(gateway).to.exist;
+    expect(is(gateway, 'bpmn:ExclusiveGateway')).to.be.true;
+
+    shapes.forEach(shape => {
+      expect(shape.outgoing[0].target).to.eql(gateway);
+    });
+
+    expect(gateway.x).to.eql(525);
+    expect(gateway.y).to.eql(92);
+  }));
 });
 
 function element(id) {
