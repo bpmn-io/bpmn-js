4f188f9162d3a14bb4d26e98760080ef6e5d8a5f
Daniel <150448993+sombrek@users.noreply.github.com>
2024-01-04 14:17:07 +0100

    feat: resize vertical pools and lanes using handles

--- test/spec/features/modeling/behavior/ResizeBehaviorSpec.js
+++ test/spec/features/modeling/behavior/ResizeBehaviorSpec.js
@@ -155,6 +155,131 @@ describe('features/modeling - resize behavior', function() {
     });
 
 
+    describe('vertical minimum dimensions', function() {
+
+      var diagramXML = require('./ResizeBehavior.participant.vertical.bpmn');
+
+      beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
+
+
+      it('should snap to min dimensions from <se>', inject(
+        function(dragging, elementRegistry, resize) {
+
+          // given
+          var participant = elementRegistry.get('Vertical_Participant_1');
+
+          // when
+          resize.activate(canvasEvent({ x: 500, y: 500 }), participant, 'se');
+
+          dragging.move(canvasEvent({ x: 0, y: 0 }));
+
+          dragging.end();
+
+          // then
+          expect(participant.width).to.equal(60);
+          expect(participant.height).to.equal(300);
+        })
+      );
+
+
+      it('should snap to min dimensions from <nw>', inject(
+        function(dragging, elementRegistry, resize) {
+
+          // given
+          var participant = elementRegistry.get('Vertical_Participant_1');
+
+          // when
+          resize.activate(canvasEvent({ x: 0, y: 0 }), participant, 'nw');
+
+          dragging.move(canvasEvent({ x: 500, y: 500 }));
+
+          dragging.end();
+
+          // then
+          expect(participant.width).to.equal(60);
+          expect(participant.height).to.equal(300);
+        })
+      );
+
+
+      it('should snap to children from <se>', inject(function(dragging, elementRegistry, resize) {
+
+        // given
+        var participant = elementRegistry.get('Vertical_Participant_2');
+
+        // when
+        resize.activate(canvasEvent({ x: 500, y: 500 }), participant, 'se');
+
+        dragging.move(canvasEvent({ x: 0, y: 0 }));
+
+        dragging.end();
+
+        // then
+        expect(participant.width).to.equal(293);
+        expect(participant.height).to.equal(433);
+      }));
+
+
+      it('should snap to children from <nw>', inject(function(dragging, elementRegistry, resize) {
+
+        // given
+        var participant = elementRegistry.get('Vertical_Participant_2');
+
+        // when
+        resize.activate(canvasEvent({ x: 0, y: 0 }), participant, 'nw');
+
+        dragging.move(canvasEvent({ x: 500, y: 500 }));
+
+        dragging.end();
+
+        // then
+        expect(participant.width).to.equal(279);
+        expect(participant.height).to.equal(467);
+      }));
+
+
+      it('should snap to min dimensions + children from <se>', inject(
+        function(dragging, elementRegistry, resize) {
+
+          // given
+          var participant = elementRegistry.get('Vertical_Participant_3');
+
+          // when
+          resize.activate(canvasEvent({ x: 500, y: 500 }), participant, 'se');
+
+          dragging.move(canvasEvent({ x: 0, y: 0 }));
+
+          dragging.end();
+
+          // then
+          expect(participant.width).to.equal(163);
+          expect(participant.height).to.equal(345);
+        })
+      );
+
+
+      it('should snap to min dimensions + children from <nw>', inject(
+        function(dragging, elementRegistry, resize) {
+
+          // given
+          var participant = elementRegistry.get('Vertical_Participant_3');
+
+          // when
+          resize.activate(canvasEvent({ x: 0, y: 0 }), participant, 'nw');
+
+          dragging.move(canvasEvent({ x: 500, y: 500 }));
+
+          dragging.end();
+
+          // then
+          expect(participant.width).to.equal(177);
+          expect(participant.height).to.equal(353);
+        })
+      );
+
+    });
+
+
     describe('resize constraints', function() {
 
       var diagramXML = require('./ResizeBehavior.lanes.bpmn');
@@ -196,8 +321,57 @@ describe('features/modeling - resize behavior', function() {
           dragging.end();
 
           // then
-          expect(lane.width).to.equal(313);
+          expect(lane.width).to.equal(330);
           expect(lane.height).to.equal(122);
+        })
+      );
+
+    });
+
+
+    describe('vertical resize constraints', function() {
+
+      var diagramXML = require('./ResizeBehavior.lanes.vertical.bpmn');
+
+      beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
+
+
+      it('should snap to child lanes from <nw>', inject(
+        function(dragging, elementRegistry, resize) {
+
+          // given
+          var participant = elementRegistry.get('Vertical_Participant');
+
+          // when
+          resize.activate(canvasEvent({ x: 0, y: 0 }), participant, 'nw');
+
+          dragging.move(canvasEvent({ x: 500, y: 500 }));
+
+          dragging.end();
+
+          // then
+          expect(participant.width).to.equal(283);
+          expect(participant.height).to.equal(563);
+        })
+      );
+
+
+      it('should snap to nested child lanes from <se>', inject(
+        function(dragging, elementRegistry, resize) {
+
+          // given
+          var lane = elementRegistry.get('Vertical_Lane_B_0');
+
+          // when
+          resize.activate(canvasEvent({ x: 0, y: 0 }), lane, 'se');
+
+          dragging.move(canvasEvent({ x: -500, y: -500 }));
+
+          dragging.end();
+
+          // then
+          expect(lane.width).to.equal(142);
+          expect(lane.height).to.equal(330);
         })
       );
 
@@ -266,7 +440,10 @@ var LANE_MIN_HEIGHT = 60,
     LANE_RIGHT_PADDING = 20,
     LANE_LEFT_PADDING = 50,
     LANE_TOP_PADDING = 20,
-    LANE_BOTTOM_PADDING = 20;
+    LANE_BOTTOM_PADDING = 20,
+    VERTICAL_LANE_MIN_WIDTH = 60,
+    VERTICAL_LANE_TOP_PADDING = 50,
+    VERTICAL_LANE_LEFT_PADDING = 20;
 
 describe('modeling/behavior - resize behavior - utilities', function() {
 
@@ -525,6 +702,271 @@ describe('modeling/behavior - resize behavior - utilities', function() {
         expect(sizeConstraints).to.eql({
           min: {
             right: otherShape.x + otherShape.width + LANE_RIGHT_PADDING
+          },
+          max: { }
+        });
+
+      }));
+
+    });
+
+  });
+
+  describe('vertical #getParticipantResizeConstraints', function() {
+
+    describe('lanes', function() {
+
+      var diagramXML = require('./ResizeBehavior.utility.lanes.vertical.bpmn');
+
+      beforeEach(bootstrapModeler(diagramXML, { modules: [ coreModule ] }));
+
+
+      it('resize participant (E)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Vertical_Participant_Lane'),
+            otherLaneShape = elementRegistry.get('Vertical_Lane_B');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'e');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            right: otherLaneShape.x + VERTICAL_LANE_MIN_WIDTH
+          },
+          max: {}
+        });
+
+      }));
+
+
+      it('right lane (E)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Vertical_Lane_B'),
+            otherLaneShape = elementRegistry.get('Vertical_Lane_B');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'e');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            right: otherLaneShape.x + VERTICAL_LANE_MIN_WIDTH
+          },
+          max: {}
+        });
+
+      }));
+
+
+      it('resize participant (W)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Vertical_Participant_Lane'),
+            otherLaneShape = elementRegistry.get('Nested_Vertical_Lane_A');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'w');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            left: otherLaneShape.x + otherLaneShape.width - VERTICAL_LANE_MIN_WIDTH
+          },
+          max: {}
+        });
+
+      }));
+
+
+      it('resize left lane (L)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Vertical_Lane_A'),
+            otherLaneShape = elementRegistry.get('Nested_Vertical_Lane_A');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'w');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            left: otherLaneShape.x + otherLaneShape.width - VERTICAL_LANE_MIN_WIDTH
+          },
+          max: {}
+        });
+
+      }));
+
+
+      it('resize middle lane (W)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Nested_Vertical_Lane_B'),
+            aboveLaneShape = elementRegistry.get('Nested_Vertical_Lane_A');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'w', true);
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            left: resizeShape.x + resizeShape.width - VERTICAL_LANE_MIN_WIDTH
+          },
+          max: {
+            left: aboveLaneShape.x + VERTICAL_LANE_MIN_WIDTH
+          }
+        });
+
+      }));
+
+
+      it('resize middle lane (E)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Nested_Vertical_Lane_B'),
+            otherLaneShape = elementRegistry.get('Vertical_Lane_B');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'e', true);
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            right: resizeShape.x + VERTICAL_LANE_MIN_WIDTH
+          },
+          max: {
+            right: otherLaneShape.x + otherLaneShape.width - VERTICAL_LANE_MIN_WIDTH
+          }
+        });
+
+      }));
+
+    });
+
+
+    describe('flowNodes', function() {
+
+      var diagramXML = require('./ResizeBehavior.utility.lanes.vertical-flowNodes.bpmn');
+
+      beforeEach(bootstrapModeler(diagramXML, { modules: [ coreModule ] }));
+
+
+      it('resize participant (E)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Vertical_Participant_Lane'),
+            taskShape = elementRegistry.get('Task');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'e');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            right: taskShape.x + taskShape.width + LANE_RIGHT_PADDING
+          },
+          max: {}
+        });
+
+      }));
+
+
+      it('right lane (E)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Vertical_Lane_B'),
+            taskShape = elementRegistry.get('Task');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'e');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            right: taskShape.x + taskShape.width + LANE_RIGHT_PADDING
+          },
+          max: {}
+        });
+
+      }));
+
+
+      it('resize participant (W)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Vertical_Participant_Lane'),
+            taskShape = elementRegistry.get('Task_Boundary');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'w');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            left: taskShape.x - VERTICAL_LANE_LEFT_PADDING
+          },
+          max: {}
+        });
+
+      }));
+
+
+      it('resize left lane (W)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Vertical_Lane_A'),
+            taskShape = elementRegistry.get('Task_Boundary');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'w');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            left: taskShape.x - VERTICAL_LANE_LEFT_PADDING
+          },
+          max: {}
+        });
+
+      }));
+
+
+      it('resize lane (N)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Nested_Vertical_Lane_B'),
+            otherShape = elementRegistry.get('Boundary_label');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 'n');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            top: otherShape.y - VERTICAL_LANE_TOP_PADDING
+          },
+          max: { }
+        });
+
+      }));
+
+
+      it('resize lane (S)', inject(function(elementRegistry) {
+
+        // given
+        var resizeShape = elementRegistry.get('Vertical_Lane_B'),
+            otherShape = elementRegistry.get('Task');
+
+        // when
+        var sizeConstraints = getParticipantResizeConstraints(resizeShape, 's');
+
+        // then
+        expect(sizeConstraints).to.eql({
+          min: {
+            bottom: otherShape.y + otherShape.height + LANE_BOTTOM_PADDING
           },
           max: { }
         });
