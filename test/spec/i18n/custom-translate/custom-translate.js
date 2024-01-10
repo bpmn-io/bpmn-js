2d0fe7169678c98c6bd6758bee0f7c0cb9803166
Beatriz Mendes <beatriz.mendes@camunda.com>
2024-01-10 14:50:50 +0100

    chore(context-pad): user friendly tooltip text

--- test/spec/i18n/custom-translate/custom-translate.js
+++ test/spec/i18n/custom-translate/custom-translate.js
@@ -1,7 +1,7 @@
 import translate from 'diagram-js/lib/i18n/translate/translate';
 
 export default function customTranslate(template, replacements) {
-  if (template === 'Remove') {
+  if (template === 'Delete') {
     template = 'Entfernen';
   }
 
