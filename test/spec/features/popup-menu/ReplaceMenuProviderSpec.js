8e1141125c69e93d357dc4515a574d62e21bab94
Jarek Danielak <jarek.danielak@camunda.com>
2026-02-27 12:53:35 +0100

    fix: explicitly set `isInterrupting` for events in the replace menu
    Closes https://github.com/bpmn-io/bpmn-js/issues/2313
    Related to https://github.com/camunda/camunda-modeler/issues/5395

--- test/spec/features/popup-menu/ReplaceMenuProviderSpec.js
+++ test/spec/features/popup-menu/ReplaceMenuProviderSpec.js
@@ -1056,6 +1056,28 @@ describe('features/popup-menu - replace menu provider', function() {
       })
     );
 
+
+    it('should replace interrupting event <-> non-interrupting event',
+      inject(function(elementRegistry) {
+
+        // given
+        const event = elementRegistry.get('MessageStartEvent');
+
+        // when
+        openPopup(event);
+        const nonInterruptingEvent = triggerAction('replace-with-non-interrupting-message-start');
+
+        // then
+        expect(nonInterruptingEvent.businessObject.isInterrupting, 'isInterrupting').to.be.false;
+
+        // when
+        openPopup(nonInterruptingEvent);
+        const interruptingEvent = triggerAction('replace-with-message-start');
+
+        // then
+        expect(interruptingEvent.businessObject.isInterrupting, 'isInterrupting').to.be.true;
+      })
+    );
   });
 
 
